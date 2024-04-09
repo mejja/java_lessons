c86454dbb4237f01af947d9e191e83e95efa0c63
mejja <majorkiema.mk@gmail.com>
2024-04-09 10:57:04 +0100

    3.4_kewords/keywords.java

---
+++ 3.4_keywords/Keywords.java
@@ -0,0 +1,7 @@
+public class Keywords{
+	public static void main(String[] args){
+		String phrase="The Java language was developed back in ";
+		int year= 1995;
+		System.out.println(phrase + year);
+	}   
+}
