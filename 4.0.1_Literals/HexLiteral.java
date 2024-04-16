b4cf67e578b6bb776880341dee7375f48483f5dc
mejja <majorkiema.mk@gmail.com>
2024-04-16 20:14:36 +0100

    4.0.1_Literals

---
+++ 4.0.1_Literals/HexLiteral.java
@@ -0,0 +1,8 @@
+public class HexLiteral{
+	public static void main(String[] args){
+		int hex1= 0X25;
+		int hex2= 0XA;
+		int product= hex1 * hex2;
+	System.out.println("The product of the two hex digits is: "+ product);
+	}
+}
