a43b9e9e140042f4b9dcda8de601d7681ab4fca0
mejja <majorkiema.mk@gmail.com>
2024-04-17 20:10:05 +0100

    added AgeCalculator.java

---
+++ 1.0_Challenge_1/AgeCalculator.java
@@ -0,0 +1,24 @@
+import java.util.*; //import date
+import java.time.*;
+import java.text.SimpleDateFormat;
+
+
+public class AgeCalculator{
+	public static void main(String[] args){
+		String date_of_birth = "01/01/1999"; 
+		Date SysDateObj = new Date(); //Create a date object
+		System.out.println(SysDateObj);//Print current date before formatting
+
+		SimpleDateFormat myFormatObj = new SimpleDateFormat("dd/MM/yyyy");
+
+		String formattedDate = myFormatObj.format(SysDateObj);
+		System.out.println("After Formatting: " + formattedDate);
+
+		SimpleDateFormat formatNowYear = new SimpleDateFormat("yyyy");
+		java.util.Date nowDate = new java.util.Date();
+		String currentYear = formatNowYear.format(nowDate);
+		System.out.println(currentYear);
+
+
+	}
+}
