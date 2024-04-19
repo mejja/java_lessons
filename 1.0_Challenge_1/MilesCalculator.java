6a27e5f6a93661d8932473134e5a1564fb8eeab2
mejja <majorkiema.mk@gmail.com>
2024-04-19 20:50:43 +0100

    MilesCalculator.java

---
+++ 1.0_Challenge_1/MilesCalculator.java
@@ -0,0 +1,10 @@
+public class MilesCalculator{
+	public static void main(String args[]){
+		int distance = 50;
+        
+        //distance conversion to miles
+		double distance_in_miles = distance_from_store * 0.621371;
+
+		System.out.println("The distance in Miles: " + distance_in_miles);
+	}
+}
