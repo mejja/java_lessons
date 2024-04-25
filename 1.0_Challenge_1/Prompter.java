6ce7b302bac1ce20d017641d7edea48a29eb2141
mejja <majorkiema.mk@gmail.com>
2024-04-25 21:33:19 +0100

    Prompter.java

--- 1.0_Challenge_1/Prompter.java
+++ 1.0_Challenge_1/Prompter.java
@@ -1,20 +1,9 @@
 //Import Scanner Class
 import java.util.Scanner; 
-
 public class Prompter{
-	//for string input
 	public static String prompt(String message){
 		System.out.println(message);
 		Scanner input = new Scanner(System.in);
-		String prompt = input.next();
-		return prompt;
+		 return input.nextLine();
 	}
-	//for numeric input
-    // public static String promptInt(String message){
-	// 	System.out.println(message);
-	// 	Scanner input = new Scanner(System.in);
-	// 	int promptInt = input.nextint();
-	// 	return promptInt;
-	// }
-
 }
