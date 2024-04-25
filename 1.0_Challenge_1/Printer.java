9625a89d5340a5c775917a7931b57a4c1b7c3c07
mejja <majorkiema.mk@gmail.com>
2024-04-25 21:33:42 +0100

    Printer.java

--- 1.0_Challenge_1/Printer.java
+++ 1.0_Challenge_1/Printer.java
@@ -1,14 +1,10 @@
 public class Printer{
-	public static void print( String name,  String surname,  String date_of_birth, double distance  ){
-		System.out.println("Hello " + name+ " "+ surname );
-		System.out.println("Your D.O.B is: " +  date_of_birth);
-		System.out.println("The distance to you favourite store in miles is:" + distance*0.621371  + " " + "miles");
+	public static void print( String name, String surname, long age, double distance_in_miles){
+		System.out.println("Hello "+ name + " "+surname+"!!");
+		System.out.println("You are " + age+" years old" );
+		System.out.println("The distance form the shop is " + distance_in_miles +" miles" );
+		//Brownie points
+		System.out.println("The age in seconds is "+ age*31_556_952  + " Secs Old" );
 	}
+
 }
-		
-		// System.out.println("You are "+ (years *31556952)  +" "+ "Seconds old");
-		// System.out.println("You Age in Hexadecimal is: "+ Integer.toHexString(years)  + " Hex years old");
-		// System.out.println("You Age in Binary is: "+ Integer.toBinaryString(years)  + "  years old");
-		// //Distance
-		// System.out.println("The distance to you favourite store in miles is:" + distance_in_miles  + " " + "miles");
-		// System.out.println("The distance to you favourite store in meters is:" + distance_in_miles*1609.34 + " "+ "metres");
