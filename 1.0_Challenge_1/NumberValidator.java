04bcdc561691b74818743adbc1c2467d6ee91ea6
mejja <majorkiema.mk@gmail.com>
2024-04-25 21:32:52 +0100

    NumberValidator.java

--- 1.0_Challenge_1/NumberValidator.java
+++ 1.0_Challenge_1/NumberValidator.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
-
 public class NumberValidator{
-	public static boolean NumIsValid(int distance_from_store){
-		Scanner sc = new Scanner(System.in);
-		do{
-			// System.out.println("How far is your favourite store from your home?");
-			while(!sc.hasNextInt()){
-				System.out.println("That's not a number!");
-				sc.next();
-			}
-
-		distance_from_store= sc.nextInt();
-		}while (distance_from_store <= 0);
-		System.out.println("Thank you! Got " + distance_from_store);
-		return false;
-		
-	}
+    public static boolean numIsValid(String distance_from_store) {
+    int intValue;
+        
+    //System.out.println(String.format("Parsing string: \"%s\"", string));
+        
+    if(distance_from_store == null || distance_from_store.equals("")) {
+        System.out.println("String cannot be parsed, it is null or empty.");
+        return false;
+    }
+    
+    try {
+        intValue = Integer.parseInt(distance_from_store);
+        return true;
+    } catch (NumberFormatException e) {
+    }
+    return false;
 }
+}
+
