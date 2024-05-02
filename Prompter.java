c570ef40276cac8156a270056b0fdcd3a48020fc
mejja <majorkiema.mk@gmail.com>
2024-05-02 19:18:31 +0100

    Prompter.java

---
+++ Prompter.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; 
+public class Prompter{
+	public static String prompt(String message){
+		System.out.println(message);
+		Scanner input = new Scanner(System.in);
+		 return input.nextLine();
+}
+}
