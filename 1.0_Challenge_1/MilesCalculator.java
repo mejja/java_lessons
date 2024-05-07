441578f82a6eeebb1037ffc8f7d6f48413b779e7
mejja <majorkiema.mk@gmail.com>
2024-05-07 23:38:56 +0100

    updated MilesCalculator.java

--- 1.0_Challenge_1/MilesCalculator.java
+++ 1.0_Challenge_1/MilesCalculator.java
@@ -1,6 +1,5 @@
 public class MilesCalculator{
 	public static double calc(double distance_from_store){
-        //distance conversion to miles
 		double distance_in_miles = distance_from_store * 0.621371;
 		return distance_in_miles;
 	}
