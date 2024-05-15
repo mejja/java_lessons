5875fa3e53eb8297b6a09879b3baa8be2e466502
mejja <majorkiema.mk@gmail.com>
2024-05-15 22:59:05 +0100

    Halfway exam1/Question18.java

---
+++ POST-Moterm/Halfway exam1/Question18.java
@@ -0,0 +1,9 @@
+public class Question18{
+	public static void main(String[] args){
+		int x=2;
+		x= x++;
+		//int x= +3;//error try to defined a defined variable
+		System.out.println(x);
+	
+	}
+}
