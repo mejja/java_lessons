61f758f812c5f1b7aa7f7c494b53bad1016a6479
mejja <majorkiema.mk@gmail.com>
2024-05-20 22:15:53 +0100

    Halfway exam1/Question20.java

---
+++ POST-Moterm/Halfway exam1/Question20.java
@@ -0,0 +1,7 @@
+public class Question20{
+	public static void main(String args[]){
+		double k = 2.000;
+		System.out.println( (long) k*0.1);//narrowing is happening before widening to double again
+	
+	}
+}
