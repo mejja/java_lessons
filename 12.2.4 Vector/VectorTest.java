1abe8ce3b69fad7b432bd097e60f412ef9d473c2
mejja <majorkiema.mk@gmail.com>
2024-05-28 16:30:51 +0100

    12.2.4 Vector

---
+++ 12.2.4 Vector/VectorTest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class VectorTest {
+    public static void main(String[] args) {
+
+        Vector<String> v = new Vector<>();
+        
+        v.add("banana");
+        v.add("apples");
+        v.add("Pineapples");
+        v.add("mangoes");
+        v.add("oranges");
+        v.add("watermelon");
+        v.add("grapes");
+           
+        System.out.println(v);
+        
+    }
+}
