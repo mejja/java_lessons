27272325452432aee12208705dd4ba9b81e069c1
mejja <majorkiema.mk@gmail.com>
2024-05-29 12:42:11 +0100

    12.2.6 HashMap

---
+++ 12.2.6 HashMap/MapTest1.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class MapTest1 {
+	public static void main(String[] args) {
+		HashMap hashMap = new HashMap();
+		
+		hashMap.put("key1", "A");
+		hashMap.put("key2", "B");
+		hashMap.put("key3", "C");
+		hashMap.put("key4", "D");
+		hashMap.put("key5", "E");
+		hashMap.put("key6", "F");
+		
+		Set keySet = hashMap.keySet();		
+		Collection values = hashMap.values();		
+		
+	}	
+}
