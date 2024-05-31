05a9dac744c4f2e56fa8ff0c7e71e1e42edd2133
mejja <majorkiema.mk@gmail.com>
2024-05-31 21:37:01 +0100

    13.6.1 Assert

---
+++ 13.6.1 Assert/AssertTest.java
@@ -0,0 +1,9 @@
+public class AssertTest {
+    public static void main(String[] args) {
+        boolean condtion = true;
+       
+        assert condtion : "expected true condition";
+       
+        System.out.println("Finished");
+    }
+}
