479f68eb54d82aa4390ea5da35740e28dc31bd83
mejja <majorkiema.mk@gmail.com>
2024-06-19 21:04:45 +0100

    12.2.3  ArrayList/ArrayListTest4.java

---
+++ 12.2.3  ArrayList/ArrayListTest4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ArrayListTest4 {
+    public static void main(String[] args) {
+
+        ArrayList<String> name = new ArrayList<>();
+        
+        name.add("Major");
+        name.add("Kiema");
+        name.add("Mbandi");
+           
+            System.out.println(name);
+
+        name.clear();
+
+            System.out.println(name);
+        
+    }
+}
