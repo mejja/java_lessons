b56fc467fea0c70d9c5dc313bba138f3e67f5eb4
Major Kiema Mbandi <majorkiema.mk@gmail.com>
2024-06-30 17:32:05 +0100

    Update ObjectB.java

--- OOP/OOP 1.3 Class and Class loaders/ObjectB.java
+++ OOP/OOP 1.3 Class and Class loaders/ObjectB.java
@@ -1,6 +1,7 @@
 public class ObjectB {
 	public void add(int val1,int val2, int val3){
 		int sum = val1+val2+val3;
+  System.out.println("Sum");
 		System.out.println("val1 + val2 + val3= "+sum);
 	}
 }
