f1712f282c446b65acfdad39d8199cffe3e8671e
mejja <majorkiema.mk@gmail.com>
2024-07-09 18:06:02 +0100

    Modified POST-Moterm/Halfway exam1/Question04.java

--- POST-Moterm/Halfway exam1/Question04.java
+++ POST-Moterm/Halfway exam1/Question04.java
@@ -2,6 +2,7 @@ public class Question04{
 	public static void main(String args[]){
 	double i =3.0;
 	String j= "1";
-	System.out.println(i+(j)+8*(-3));
+	String k= "One";
+	System.out.println(i+(j)+k+8*(3));//3.01One24
 	}
 }
