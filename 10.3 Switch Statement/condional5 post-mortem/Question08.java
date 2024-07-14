5271e1da1ba5d3adfe751568a18d614b1493a07c
mejja <majorkiema.mk@gmail.com>
2024-07-14 22:49:59 +0100

    10.3 Switch Statement/condional5 post-mortem/Question08.java

---
+++ 10.3 Switch Statement/condional5 post-mortem/Question08.java
@@ -0,0 +1,12 @@
+public class Question08{
+	public static void main(String args[]){
+	int i =10, j =0;
+	i--;
+	if(i > 0)
+	{
+		System.out.println(i + " ");
+		System.out.println(j++ + --i);
+		--i;
+	}
+	}
+}
