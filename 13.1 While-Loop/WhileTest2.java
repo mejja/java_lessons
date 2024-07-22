40ae52a70ec7597fbbc614fa2cde077958b93eb1
mejja <majorkiema.mk@gmail.com>
2024-07-22 20:24:19 +0100

    Added 13.1 While-Loop/WhileTest2.java

---
+++ 13.1 While-Loop/WhileTest2.java
@@ -0,0 +1,9 @@
+public class WhileTest2 {
+    public static void main(String[] args) {
+        int counter = 0;      
+        while(counter++ < 10){
+            if(counter%2== 0)
+            System.out.println("Counter = "+counter);
+        }
+    }
+}
