a1435276171ab7d7ac8bb03483eac42ef84c338b
mejja <majorkiema.mk@gmail.com>
2024-07-24 18:52:27 +0100

    Added POST-Moterm/Halfway exam1/QuestionXX.java

---
+++ POST-Moterm/Halfway exam1/QuestionXX.java
@@ -0,0 +1,18 @@
+public class QuestionXX{
+	public static void main(String args[]){
+		double i = (int) 'A' + 145f + 23D;
+		byte b =100;
+		b += 8;
+		
+		int a =0152;
+		a *= --a/2 *2;
+		
+		char A = '\u0005';
+		if(A == 0x0005L)
+			System.out.println(a);
+
+
+	
+	
+	}
+}
