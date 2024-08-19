4c84be2ff22972842e8c1fe7b453c4bc3dd4d404
mejja <majorkiema.mk@gmail.com>
2024-08-19 18:41:03 +0100

    deleted: 10.2 If-Statement Advanced/IfTest9.java

--- 10.2 If-Statement Advanced/IfTest9.java
+++
@@ -1,9 +0,0 @@
-public class IfTest9 {
-    public static void main(String[] args) {
-        int a = 4;
-        int b = 6;
-       
-        String message = a >= b ? "Cool" : "Not Cool";       
-        System.out.println(message);
-    }
-}
