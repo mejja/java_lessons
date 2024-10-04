76fe82986719e44442175d036c25b80ece28a349
mejja <majorkiema.mk@gmail.com>
2024-10-04 12:57:22 +0100

    deleted: 13.2 Do-While Loop/DoWhileTest1.java

--- 13.2 Do-While Loop/DoWhileTest1.java
+++
@@ -1,8 +0,0 @@
-public class DoWhileTest1 {
-    public static void main(String[] args) {
-        int counter = 0;      
-        do{
-            System.out.println("Counter = "+counter);
-        }while(counter++ < 10);
-    }
-}
