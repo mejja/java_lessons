6e8d5d62c75b272c381b2691af2923b846cd2928
mejja <majorkiema.mk@gmail.com>
2024-10-07 05:38:12 +0100

    deleted: 13.6.1 Assert/AssertTest.java

--- 13.6.1 Assert/AssertTest.java
+++
@@ -1,9 +0,0 @@
-public class AssertTest {
-    public static void main(String[] args) {
-        boolean condtion = true;
-       
-        assert condtion : "expected true condition";
-       
-        System.out.println("Finished");
-    }
-}
