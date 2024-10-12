a145c9df3e2d47609d62c20b3bdb5a36e776475d
mejja <majorkiema.mk@gmail.com>
2024-10-12 20:45:00 +0100

    deleted: 3.4_keywords/Keywords.java

--- 3.4_keywords/Keywords.java
+++
@@ -1,7 +0,0 @@
-public class Keywords{
-	public static void main(String[] args){
-		String phrase="The Java language was developed back in ";
-		int year= 1995;
-		System.out.println(phrase + year);
-	}   
-}
