eb02ad6a652dbe3b9525e027ff68abfb454e0092
mejja <majorkiema.mk@gmail.com>
2024-10-18 11:16:39 +0100

    deleted:4.0.1_Literals/HexLiteral.java

--- 4.0.1_Literals/HexLiteral.java
+++
@@ -1,8 +0,0 @@
-public class HexLiteral{
-	public static void main(String[] args){
-		int hex1= 0X25;
-		int hex2= 0XA;
-		int product= hex1 * hex2;
-	System.out.println("The product of the two hex digits is: "+ product);
-	}
-}
