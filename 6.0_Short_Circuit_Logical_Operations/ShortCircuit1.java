f3c361f07e629b35a7dc1303e53f2cbb13afffce
mejja <majorkiema.mk@gmail.com>
2024-10-22 22:09:59 +0100

    6.0_Short_Circuit_Logical_Operations/ShortCircuit1.java

--- 6.0_Short_Circuit_Logical_Operations/ShortCircuit1.java
+++
@@ -1,8 +0,0 @@
-public class ShortCircuit1 {
-    public static void main(String[] args) {
- 	   boolean a = false;
-        boolean b = false;
-       
-        System.out.println(a && b);
-    }
-}
