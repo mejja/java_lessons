898df1ee53577fc7bc5862284d03354eb2d7367d
mejja <majorkiema.mk@gmail.com>
2024-10-23 07:52:31 +0100

    deleted: 6.0_Short_Circuit_Logical_Operations/ShortCircuit3.java

--- 6.0_Short_Circuit_Logical_Operations/ShortCircuit3.java
+++
@@ -1,8 +0,0 @@
-public class ShortCircuit3 {
-    public static void main(String[] args) {
-        boolean a = false;
-        System.out.println("Before a ="+a);
-        a = !a;   
-        System.out.println("After a ="+a);
-    }
-}
