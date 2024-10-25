f92d36a87cb324381e2e7dc551b1b039efcb7942
mejja <majorkiema.mk@gmail.com>
2024-10-25 08:06:03 +0100

    deleted: 7.1_Assignment_Operators/AssigmentOp2.java

--- 7.1_Assignment_Operators/AssigmentOp2.java
+++
@@ -1,8 +0,0 @@
-public class AssigmentOp2 {
-    public static void main(String[] args) {
-        int myInt1 = 5;
-        int myInt2 = 2;
-        myInt1+=myInt2;    
-        System.out.println(myInt1);
-    }
-}
