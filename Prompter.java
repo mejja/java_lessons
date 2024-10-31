fb678f5c3e518a09389429e1916b7b7a975f37de
mejja <majorkiema.mk@gmail.com>
2024-10-31 06:40:55 +0000

    Deleted: Prompter.java

--- Prompter.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner; 
-public class Prompter{
-	public static String prompt(String message){
-		System.out.println(message);
-		Scanner input = new Scanner(System.in);
-		 return input.nextLine();
-}
-}
