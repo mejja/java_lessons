0b4e8b11c7e5bed2b608f83993b3e3626a4fcfed
mejja <majorkiema.mk@gmail.com>
2024-11-01 06:06:53 +0000

    deleted: challengeTest/MilesCalculator.java

--- challengeTest/MilesCalculator.java
+++
@@ -1,7 +0,0 @@
-public class MilesCalculator{
-	public static double calc(double distance_from_store){
-        //distance conversion to miles
-		double distance_in_miles = distance_from_store * 0.621371;
-		return distance_in_miles;
-	}
-}
