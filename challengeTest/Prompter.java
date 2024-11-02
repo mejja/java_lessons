9593661536d0668d19f0271f965ba99703bfa390
mejja <majorkiema.mk@gmail.com>
2024-11-02 17:49:22 +0000

    deleted: challengeTest/Prompter.java

--- challengeTest/Prompter.java
+++
@@ -1,9 +0,0 @@
-//Import Scanner Class
-import java.util.Scanner; 
-public class Prompter{
-	public static String prompt(String message){
-		System.out.println(message);
-		Scanner input = new Scanner(System.in);
-		 return input.nextLine();
-	}
-}
