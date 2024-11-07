a286a638894f5a8e86bc8a9649e6e68c4ed8e91b
mejja <majorkiema.mk@gmail.com>
2024-11-07 18:35:58 +0000

    Added 12.2.2 Introduction to Lists/

---
+++ 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ArrayListTest1 {
+    public static void main(String[] args) {
+
+        List<Integer> l1 = new ArrayList<Integer>();
+        // Adding elements to object of List interface Custom inputs 
+  
+        l1.add(0, 1); 
+        l1.add(1, 2);     
+            System.out.println(l1);
+        
+    }
+}
