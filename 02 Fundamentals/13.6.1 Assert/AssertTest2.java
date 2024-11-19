1f3becd1fa7470c3d83aa5656f931ead55a1da77
mejja <majorkiema.mk@gmail.com>
2024-11-19 07:39:07 +0000

    Added: 02 Fundamentals/13.6.1 Assert/

---
+++ 02 Fundamentals/13.6.1 Assert/AssertTest2.java
@@ -0,0 +1,9 @@
+public class AssertTest2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 4;
+        assert a>0;
+        assert b>10;
+        System.out.println("Finished");
+    }
+}
