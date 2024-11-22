5b6214d8cf1e0b43811384e8a25503233b41f531
mejja <majorkiema.mk@gmail.com>
2024-11-22 14:19:38 +0000

    02 Fundamentals/13.8 Algorithms and Pseudocode/

---
+++ 02 Fundamentals/13.8 Algorithms and Pseudocode/pseudocode.java
@@ -0,0 +1,8 @@
+// let arr1[50]
+
+// for num from 1 to 50
+// arr1.add(num)
+// number =numbers+1
+// print numbers
+
+
---
+++ 02 Fundamentals/13.8 Algorithms and Pseudocode/pseudocode2.java
@@ -0,0 +1,24 @@
+let arrMulti[] []
+
+
+//populate array
+Function
+for i from 0 to9
+	for j from 0 to 49
+		arrMulti[i][j]= random_number()
+
+// print array elements
+function declaration
+	FOR i from 0 to 9
+		For j from 0 to 49
+			print arrMulti[i][j]
+
+// sorting inner array elements
+function
+ for i from 0 to 9
+   sort(arrMulti[i])
+
+   // sorting arrays in ascending
+   function
+   for j from 0 to 49
+   sort(arramulti)
