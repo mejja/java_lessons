df5450b9886bbd11f524cd895baa2226a293d572
mejja <majorkiema.mk@gmail.com>
2024-12-02 07:35:54 +0000

    POST-Moterm/Halfway exam3/

---
+++ POST-Moterm/Halfway exam3/Question54.java
@@ -0,0 +1,10 @@
+public class Question54{
+	public static void main(String[] args){
+		float i = 3.33f;
+		double f = 4f;
+		double e = 26;
+		double test = 5 *(double)i;
+	System.out.println((char) (i+f+e));
+
+	}
+}
---
+++ POST-Moterm/Halfway exam3/Question58.java
@@ -0,0 +1,8 @@
+public class Question39{
+	public static void main(String[] args){
+		int a = 0152;//base 8 so be 106 in decimal
+		a *= --a/2 * 2;
+	System.out.println(a);
+
+	}
+}
