4c737e13f3946d24d796e64b68faea090e471403
mejja <majorkiema.mk@gmail.com>
2024-12-03 15:31:04 +0000

    POST-Moterm/FundsFull6/

---
+++ POST-Moterm/FundsFull6/Q19.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Q19 {
+    public static void main(String[] args) {
+        if(args[0].equals("open"))
+            if(args[1].equals("someone"))
+                System.out.println("Hello");
+            else System.out.println("Go away"+ args[1]);
+    }
+}
---
+++ POST-Moterm/FundsFull6/Q20.java
@@ -0,0 +1,9 @@
+public class Q20{
+	public static void main(String[] args){
+		int i =3;
+		int sum =0;
+		for(System.out.println(i);i<10;i++);
+			for(System.out.println(i);i<sum;i++)
+				System.out.println(sum);
+	}
+}
