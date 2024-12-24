94a2ac02aeb7138bbdadf13e9a06e39eda71e93f
mejja <majorkiema.mk@gmail.com>
2024-12-24 20:15:27 +0000

    06.1 java.lang/02.2 Long/

---
+++ 06.1 java.lang/02.2 Long/LongTest1.java
@@ -0,0 +1,9 @@
+public class LongTest1{
+	public static void main(String[] args){
+		String stringValue = "321";
+		long value = Long.parseLong(stringValue);
+
+		System.out.println("The String char is: " + stringValue);
+		System.out.println("The int value is: " + value);
+	}
+}
