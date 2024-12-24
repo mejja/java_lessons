1fd4286d5d6c9e7c673fca01d54a417eeb3a32b4
mejja <majorkiema.mk@gmail.com>
2024-12-24 20:16:43 +0000

    06.1 java.lang/02.3 Boolean/

---
+++ 06.1 java.lang/02.3 Boolean/BooleanTest1.java
@@ -0,0 +1,10 @@
+public class BooleanTest1 {
+    public static void main(String[] args) {
+        // Create a Boolean instance using the Boolean(String s) constructor
+        String StringValue = "true";
+        Boolean value = Boolean.valueOf(StringValue);
+
+        // Print the variable
+        System.out.println("The Boolean value is: " + value);
+    }
+}
