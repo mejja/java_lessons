b2afdb28dea418b17db17f8a4537c5ffca5ebce9
mejja <majorkiema.mk@gmail.com>
2024-12-25 20:17:50 +0000

    06.1 java.lang/02.2 Long/LongTest2.java

---
+++ 06.1 java.lang/02.2 Long/LongTest2.java
@@ -0,0 +1,9 @@
+public class LongTest2{
+	public static void main(String[] args){
+		String stringValue = "123456789";
+		Long value= Long.valueOf(stringValue);
+
+		System.out.println("The Long value is: "+ value);
+
+	}
+}
