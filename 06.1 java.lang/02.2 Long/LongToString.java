f7484d6bd15f77449d9d683e2dfbc6703d08f124
mejja <majorkiema.mk@gmail.com>
2024-12-28 19:01:18 +0000

    06.1 java.lang/02.2 Long/LongToString.java

---
+++ 06.1 java.lang/02.2 Long/LongToString.java
@@ -0,0 +1,11 @@
+public class LongToString {
+    public static void main(String[] args) {
+        Long longValue = 123456789L;
+
+        // Convert Long to String
+        String stringValue = longValue.toString();
+
+        System.out.println("The Long value is: " + longValue);
+        System.out.println("The String value is: " + stringValue);
+    }
+}
