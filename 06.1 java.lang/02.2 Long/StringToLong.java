1839e385f822b1f3ee40915e86f1466ac215c7bb
mejja <majorkiema.mk@gmail.com>
2024-12-29 14:43:14 +0000

    06.1 java.lang/02.2 Long/StringToLong.java

---
+++ 06.1 java.lang/02.2 Long/StringToLong.java
@@ -0,0 +1,9 @@
+public class StringToLong {
+    public static void main(String[] args) {
+        String stringValue = "123456789";
+        Long longValue = Long.valueOf(stringValue);
+
+        // Print the result
+        System.out.println("The Long value is: " + longValue);
+    }
+}
