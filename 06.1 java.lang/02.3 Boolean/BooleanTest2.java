5bb8b3e5fbb4b0efb2b3274cc5ee67ac09abdea6
mejja <majorkiema.mk@gmail.com>
2024-12-31 09:35:42 +0000

    06.1 java.lang/02.3 Boolean/BooleanTest2.java

---
+++ 06.1 java.lang/02.3 Boolean/BooleanTest2.java
@@ -0,0 +1,11 @@
+public class BooleanTest2{
+	public static void main(String[] args){
+        boolean primitiveValue = true;//boolean primitive
+
+        //using the Boolean(boolean value) constructor
+        Boolean value = Boolean.valueOf(primitiveValue);
+
+        // Print the variable
+        System.out.println("The Boolean value is: " + value);
+	}
+}
