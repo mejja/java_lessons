be7795f50e3e7b772fe0d67676940c3ed09822b8
mejja <majorkiema.mk@gmail.com>
2025-01-01 21:36:48 +0000

    06.1 java.lang/02.4 Double/DoubleToPrimitve.java

---
+++ 06.1 java.lang/02.4 Double/DoubleToPrimitve.java
@@ -0,0 +1,10 @@
+public class DoubleToPrimitve{
+	public static void main(String[] args){
+		Double doubleObject= 1234.56;
+
+		double doublePrimitive = doubleObject.doubleValue();
+
+		System.out.println("Double Object value is: "+ doubleObject);
+		System.out.println("primitive double value is: "+ doublePrimitive);
+	}
+}
