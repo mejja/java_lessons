f72286909746f5044f3baa18fd95e5ebfc0ad9f4
mejja <majorkiema.mk@gmail.com>
2025-01-03 19:40:17 +0000

    06.1 java.lang/02.3 Boolean/BooleanToString.java

---
+++ 06.1 java.lang/02.3 Boolean/BooleanToString.java
@@ -0,0 +1,12 @@
+public class BooleanToString {
+    public static void main(String[] args) {
+        //Boolean instance
+        Boolean booleanValue = true;
+
+        // Convertion using Boolean.toString() method
+        String stringValue = Boolean.toString(booleanValue);
+
+        System.out.println("The Boolean value is: " + booleanValue);//boolean
+        System.out.println("The String value is: " + stringValue);//string value
+    }
+}
