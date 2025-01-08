827b181ef473136ac3159fb9fcf1a9c66e56185d
mejja <majorkiema.mk@gmail.com>
2025-01-08 12:53:00 +0000

    06.1 java.lang/02.4 Double/

---
+++ 06.1 java.lang/02.4 Double/DoubleTest1.java
@@ -0,0 +1,8 @@
+public class DoubleTest1{
+	public static void main(String[] args){
+		String stringValue = "125.75";
+		Double value = new Double(stringValue);//Double(String s)
+
+        System.out.println("The Double value is: " + value);
+	}
+}
