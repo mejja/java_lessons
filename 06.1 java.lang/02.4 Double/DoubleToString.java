233df1a93d9dd38425324798f0171a487ee94b2f
mejja <majorkiema.mk@gmail.com>
2025-01-09 18:44:16 +0000

    06.1 java.lang/02.4 Double/DoubleToString.java

---
+++ 06.1 java.lang/02.4 Double/DoubleToString.java
@@ -0,0 +1,10 @@
+public class DoubleToString{
+	public static void main(String[] args){
+		Double doubleObject = 1234.75;
+
+		String valueInString= doubleObject.toString();
+
+		System.out.println("The Double Object is: "+ doubleObject);
+		System.out.println("The String char after conversion is: "+ valueInString);
+	}
+}
