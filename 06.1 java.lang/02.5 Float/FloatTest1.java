3e7e32f29455b6cdb0a138d17e418fac7aa26ecb
mejja <majorkiema.mk@gmail.com>
2025-01-14 19:42:16 +0000

    02.5 Float/

---
+++ 06.1 java.lang/02.5 Float/FloatTest1.java
@@ -0,0 +1,10 @@
+public class FloatTest1{
+	public static void main(String[]  args){
+		String value = "123.25";
+
+		Float floatObject = new Float(value);
+
+		System.out.println("String Value is: "+ value);
+		System.out.println("Float Object value is: "+ floatObject);
+	}
+}
