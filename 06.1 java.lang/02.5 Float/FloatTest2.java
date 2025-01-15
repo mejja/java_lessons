3cc3778d45856ee9c76129fad541414dda11c219
mejja <majorkiema.mk@gmail.com>
2025-01-15 16:40:18 +0000

    02.5 Float/FloatTest2.java

---
+++ 06.1 java.lang/02.5 Float/FloatTest2.java
@@ -0,0 +1,10 @@
+public class FloatTest2{
+	public static void main(String[]  args){
+		float value = 123.75f;
+
+		Float floatObject = new Float(value);
+
+		System.out.println("float primitive value is: "+ value);
+		System.out.println("Float object value is: "+ floatObject);
+	}
+}
