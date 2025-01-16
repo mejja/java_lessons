7bdaaebd748452f758ce335fe5c1fba6d7707ee1
mejja <majorkiema.mk@gmail.com>
2025-01-16 18:44:56 +0000

    06.1 java.lang/02.5 Float/FloatTest3.java

---
+++ 06.1 java.lang/02.5 Float/FloatTest3.java
@@ -0,0 +1,10 @@
+public class FloatTest3{
+	public static void main(String[]  args){
+		double value = 123.75;
+
+		Float floatObject = new Float(value);
+
+		System.out.println("double primitive value is: "+ value);
+		System.out.println("Float Object of the converted type is: "+ floatObject);
+	}
+}
