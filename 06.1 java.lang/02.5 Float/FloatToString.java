0246474eb539cee1bc7b8e4b291b3ddd134ca9cd
mejja <majorkiema.mk@gmail.com>
2025-01-17 13:58:11 +0000

    06.1 java.lang/02.5 Float/FloatToString.java

---
+++ 06.1 java.lang/02.5 Float/FloatToString.java
@@ -0,0 +1,10 @@
+public class FloatToString{
+	public static void main(String[] args){
+		Float floatObject = 123.65f;
+
+		String stringValue = floatObject.toString();
+
+		System.out.println("Float Object value is:  "+ floatObject);
+		System.out.println("String value of converted Float Object: "+ stringValue);
+	}
+}
