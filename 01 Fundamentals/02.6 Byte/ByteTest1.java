d0e542786b00a1c39a95de3ba55841c10b5f98b7
mejja <majorkiema.mk@gmail.com>
2025-01-21 05:52:24 +0000

    01 Fundamentals/02.6 Byte/

---
+++ 01 Fundamentals/02.6 Byte/ByteTest1.java
@@ -0,0 +1,10 @@
+public class ByteTest1{
+	public static void main(String[]  args){
+		byte value = 123;
+
+		Byte byteObject = new Byte(value);
+
+		System.out.println("byte primitive value is:  "+ value);
+		System.out.println("Byte Object value is: "+ byteObject);
+	}
+}
