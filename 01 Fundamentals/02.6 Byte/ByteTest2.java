8cce85e491a37b6fcde066ddd90e14ffbed2803d
mejja <majorkiema.mk@gmail.com>
2025-01-22 18:06:54 +0000

    01 Fundamentals/02.6 Byte/ByteTest2.java

---
+++ 01 Fundamentals/02.6 Byte/ByteTest2.java
@@ -0,0 +1,10 @@
+public class ByteTest2{
+	public static void main(String[]  args){
+		String value = "123";
+
+		Byte byteObject = new Byte(value);
+
+		System.out.println("String value is: "+ value);
+		System.out.println("Byte Object value is: "+ byteObject );
+	}
+}
