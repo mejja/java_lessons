0e84dfc1f10bb53ddbc3d6208c95e8cfef273ae1
mejja <majorkiema.mk@gmail.com>
2025-01-23 18:34:49 +0300

    01 Fundamentals/02.6 Byte/ByteToString.java

---
+++ 01 Fundamentals/02.6 Byte/ByteToString.java
@@ -0,0 +1,10 @@
+public class ByteToString{
+	public static void main(String[]  args){
+		Byte byteObj = 123;
+
+		String stringValue = byteObj.toString();
+
+		System.out.println("Byte Object value is: "+ byteObj);
+		System.out.println("String Value is: "+ stringValue);
+	}
+}
