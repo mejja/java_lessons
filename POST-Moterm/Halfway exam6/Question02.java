b3c8afa9b65b5ed32abef86d759b3c9a052e7a92
mejja <majorkiema.mk@gmail.com>
2025-01-29 20:37:52 +0000

    Halfway exam6

---
+++ POST-Moterm/Halfway exam6/Question02.java
@@ -0,0 +1,11 @@
+public class Question02{
+	public static void main(String[] args){
+		int a = 5;
+		long b = 5;
+		boolean c =a++ ==b;
+
+		if(c);;//two one terminate the if statement & other break it
+			else
+			System.out.println("Statement is: false");
+	}
+}
