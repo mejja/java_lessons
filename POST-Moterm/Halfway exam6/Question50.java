4244e246d6dbbd974813dafc3e3241ca5d55d884
mejja <majorkiema.mk@gmail.com>
2025-02-05 06:24:29 +0000

    POST-Moterm/Halfway exam6/Question50.java

---
+++ POST-Moterm/Halfway exam6/Question50.java
@@ -0,0 +1,13 @@
+public class Question50{
+	public static void main(String[] args){
+		String s1 = "YES";
+		String s2 = "YES";
+
+		if( s1 == s2)
+			System.out.println("equal");
+		String s3 = new String("YES");
+		String s4 = new String("YES");
+		if (s3 == s4)            
+		System.out.println("s3 eq s4");
+	}
+}
