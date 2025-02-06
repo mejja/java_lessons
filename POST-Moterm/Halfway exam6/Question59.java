959811995698429ee771c159189d91a6c9a74288
mejja <majorkiema.mk@gmail.com>
2025-02-06 07:59:32 +0000

    POST-Moterm/Halfway exam6/Question59.java

---
+++ POST-Moterm/Halfway exam6/Question59.java
@@ -0,0 +1,14 @@
+public class Question59{
+	public static void main(String[] args){
+		int x = 5;
+		int y = 4;
+
+		If((y==x++) | (x<y++){
+			System.out.println(x);
+		}
+		else{
+			System.out.println(y);
+		}
+		System.out.println(x+y);
+	}
+}
