ac5594ff00b82bc6a11dc4fa53abbb2909996b1f
mejja <majorkiema.mk@gmail.com>
2025-02-07 19:38:45 +0000

    POST-Moterm/Halfway exam6/Question60.java

---
+++ POST-Moterm/Halfway exam6/Question60.java
@@ -0,0 +1,10 @@
+public class Question60{
+	public static void main(String[] args){
+		char a= 98;
+		char c = 54;
+		char d = 'A'+'B'; //compiles
+		char e= (char)(a+c);//compiles
+		            
+		System.out.println(d);
+	}
+}
