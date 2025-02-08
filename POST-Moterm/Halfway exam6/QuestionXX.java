875f59ead927881de58c85201735d80e446cfa03
mejja <majorkiema.mk@gmail.com>
2025-02-08 16:23:08 +0000

    POST-Moterm/Halfway exam6/QuestionXX.java

---
+++ POST-Moterm/Halfway exam6/QuestionXX.java
@@ -0,0 +1,17 @@
+public class QuestionXX{
+	public static void main(String[] args){
+	int i = 4;
+	double b = i++;
+	double c = ++b;
+	double d = i++;
+
+	if(i ==d)
+		System.out.println(i);
+	if(b ==i)
+		System.out.println(d);
+	else
+	System.out.println(c);
+	System.out.println(i);
+	 }
+}
+
