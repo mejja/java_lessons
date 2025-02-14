7388c7c947aa39438a89830b41f593148028897d
mejja <majorkiema.mk@gmail.com>
2025-02-14 19:25:07 +0000

    Added 04.1 Strings/

---
+++ 01 Fundamentals/04.1 Strings/StringTest1.java
@@ -0,0 +1,12 @@
+ public class StringTest1 {
+	public static void main(String[] args) {
+		String name = new String("Hello");
+		
+		if(name.contains("Hello")){
+			System.out.println("true");
+		}
+		else{
+			System.out.println("false");
+		}
+	}
+}
