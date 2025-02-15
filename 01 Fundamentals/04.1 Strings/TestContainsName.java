90ec1196cc10e7bda90f77ad7878c3a5e5bf509a
mejja <majorkiema.mk@gmail.com>
2025-02-15 19:24:06 +0000

    01 Fundamentals/04.1 Strings/TestContainsName.java

---
+++ 01 Fundamentals/04.1 Strings/TestContainsName.java
@@ -0,0 +1,12 @@
+ public class TestContainsName {
+	public static void main(String[] args) {
+		String name = new String("Major");
+		
+		if(name.contains("Major")){
+			System.out.println("true");
+		}
+		else{
+			System.out.println("false");
+		}
+	}
+}
