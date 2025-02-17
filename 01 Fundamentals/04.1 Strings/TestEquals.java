e7fd7baf0a530dcf5afc37e2cc77d6a9cc2e6be3
mejja <majorkiema.mk@gmail.com>
2025-02-17 19:45:07 +0000

    01 Fundamentals/04.1 Strings/TestEquals.java

---
+++ 01 Fundamentals/04.1 Strings/TestEquals.java
@@ -0,0 +1,12 @@
+ public class TestEquals {
+	public static void main(String[] args) {
+		String name = new String("Major");
+		
+		if(name.equals("Major")){
+			System.out.println("true");//print true
+		}
+		else{
+			System.out.println("false");
+		}
+	}
+}
