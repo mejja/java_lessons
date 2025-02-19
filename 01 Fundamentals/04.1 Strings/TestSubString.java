16999e00b9451d8218b304636f184e97b8b55399
mejja <majorkiema.mk@gmail.com>
2025-02-19 13:58:44 +0000

    01 Fundamentals/04.1 Strings/TestSubString.java

---
+++ 01 Fundamentals/04.1 Strings/TestSubString.java
@@ -0,0 +1,7 @@
+ public class TestSubString {
+	public static void main(String[] args) {
+		String name = new String("MajorKiemaMbandi");
+		
+		System.out.println(name.substring(5,10));//prints Kiema
+	}
+}
