0be1443ef593e9468899a11520e9d739a9eb4c47
mejja <majorkiema.mk@gmail.com>
2025-02-20 20:17:31 +0000

    01 Fundamentals/04.1 Strings/TestStartsWith.java

---
+++ 01 Fundamentals/04.1 Strings/TestLength.java
@@ -0,0 +1,7 @@
+ public class TestLength {
+	public static void main(String[] args) {
+		String name = new String("Major");
+		
+		System.out.println(name.length());//prints 5
+	}
+}
---
+++ 01 Fundamentals/04.1 Strings/TestStartsWith.java
@@ -0,0 +1,12 @@
+ public class TestStartsWith {
+	public static void main(String[] args) {
+		String name = new String("Major");
+		
+		if(name.startsWith("M")){
+			System.out.println("true");
+		}
+		else{
+			System.out.println("false");
+		}
+	}
+}
