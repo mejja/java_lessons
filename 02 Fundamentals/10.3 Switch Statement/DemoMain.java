8b54dd6d764144eb369d65209970832004c473dd
mejja <majorkiema.mk@gmail.com>
2025-02-21 20:43:26 +0000

    02 Fundamentals/10.3 Switch Statement/DemoMain.java

--- 02 Fundamentals/10.3 Switch Statement/DemoMain.java
+++ 02 Fundamentals/10.3 Switch Statement/DemoMain.java
@@ -1,9 +1,16 @@
-public class DemoMain{
-	public static void main(String[] args){
-	
-	if (x<0);
-	else
-	System.out.println("positive");
-	System.out.println("negative");
-	}
+public class DemoMain {  
+    public static void main(String[] args) {
+    
+        // Check if the variable 'x' is less than 0 (the variable 'x' is not defined in the code)
+        // This will result in a compilation error because 'x' is undeclared
+        if (x < 0); // <-- Unnecessary semicolon here, it makes the 'if' condition do nothing.
+
+        else
+            // If 'x' is not less than 0, print "positive"
+            System.out.println("positive");  
+
+        // Regardless of the 'if' condition, this line will always execute, printing "negative"
+        // However, this will cause confusion due to the missing braces for the 'if-else' block
+        System.out.println("negative");
+    }
 }
