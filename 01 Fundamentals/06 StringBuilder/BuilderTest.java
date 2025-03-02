32f218834b7e54c33799a41e849a3b27f667ce83
mejja <majorkiema.mk@gmail.com>
2025-03-02 20:01:21 +0000

    01 Fundamentals/06 StringBuilder/

---
+++ 01 Fundamentals/06 StringBuilder/BuilderTest.java
@@ -0,0 +1,9 @@
+public class BuilderTest{
+	public static void main(String[] args){
+		StringBuilder fullName = new StringBuilder();
+
+		fullName.append("My Name is Major Mbandi");
+
+		System.out.println(fullName.toString());
+	}
+}
