b48a164a61a21e6e34385409a098c15b12d8efad
mejja <majorkiema.mk@gmail.com>
2025-03-26 10:08:29 +0000

    02 Fundamentals/12.1.2-Label Break Continue/BlockTest2.java

--- 02 Fundamentals/12.1.2-Label Break Continue/BlockTest2.java
+++ 02 Fundamentals/12.1.2-Label Break Continue/BlockTest2.java
@@ -4,11 +4,9 @@ public class BlockTest2 {
            int i =1;
            System.out.println("Block 1 i = "+ i);
         }
-       
         label2:{
             int i =1;
             System.out.println("Block 2 i = "+ i);            
         }
-       
     }
 }
