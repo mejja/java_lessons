943e38888f2e4ef4fb650c65b75e42a06106e79d
mejja <majorkiema.mk@gmail.com>
2025-03-27 10:09:37 +0000

    refactored BlockTest3.java file

--- 02 Fundamentals/12.1.2-Label Break Continue/BlockTest3.java
+++ 02 Fundamentals/12.1.2-Label Break Continue/BlockTest3.java
@@ -1,14 +1,13 @@
 public class BlockTest3 {
     public static void main(String[] args) {
         System.out.println("Step 1");
-       
         label1: {
             System.out.println("Step 2");
             label2: {
                 System.out.println("Step 3");
                 if(true){
                     break label1;
-                }               
+                }
             }
             System.out.println("Step 4");
         }
