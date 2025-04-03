c465a13e32c580066f1d7d11766f01a95453a42b
mejja <majorkiema.mk@gmail.com>
2025-04-03 10:51:15 +0100

    02 Fundamentals/14 Challenge 2 Menu Driven Calculator/Challenge2main.java

--- 02 Fundamentals/14 Challenge 2 Menu Driven Calculator/Challenge2main.java
+++ 02 Fundamentals/14 Challenge 2 Menu Driven Calculator/Challenge2main.java
@@ -2,8 +2,8 @@
 public class Challenge2main{
 	public static void main(String[] args){
 		//invoke the prompter class
-		double op1 = Prompter.prompt("Enter the first Number!");
+		double op1 = Prompter.prompt("Enter the first N0!");
 		char operator = Prompter.promptOper("Enter the Operator: (+, -, *, /)");
-		double op2 = Prompter.prompt("Enter the second Number!");
+		double op2 = Prompter.prompt("Enter the second N0!);
 	}
 }
