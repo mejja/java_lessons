76fd900a9071b49411fac5dc9b72ab8132701b71
mejja <majorkiema.mk@gmail.com>
2025-04-09 11:16:26 +0100

    added 04. LocalTime

---
+++ 06.2 java.util/04 (LocalTime)/LocalDateTest.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+
+public class LocalDateTest {
+    public static void main(String[] args) {
+   		 LocalTime currentTime =LocalTime.now();   		 
+   		 System.out.println("today’s Date is : "+ currentTime) ;
+   		 
+    }
+}
