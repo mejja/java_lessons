fecc82c241c646be2a2cfeb31b3209dda57a584d
mejja <majorkiema.mk@gmail.com>
2025-04-21 20:54:29 +0100

    02 Fundamentals/12.1.2-Label Break Continue/BreakTest2.java

--- 02 Fundamentals/12.1.2-Label Break Continue/BreakTest2.java
+++ 02 Fundamentals/12.1.2-Label Break Continue/BreakTest2.java
@@ -1,13 +1,10 @@
 public class BreakTest2 {
     public static void main(String[] args) {
-       
         int limit = 8;
         int i=0;
-       
         while(i < limit){
             System.out.println("Counter ="+i);
             i++;
-            }       
-       
+            }
     }
 }
