2ceefb60f2a8e5114c54761e2b752977be802362
mejja <majorkiema.mk@gmail.com>
2025-04-24 13:38:15 +0100

    02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest2.java

--- 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest2.java
+++ 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest2.java
@@ -5,7 +5,7 @@ public class ArrayListTest2 {
 
         List<String> names = new ArrayList<String>();
         // Adding elements to object of List interface Custom inputs
-        names.add(0, "Major"); 
+        names.add(0, "Major");
         names.add(1, "Mbandi");
         names.add(2, "Amy");
         names.add(3, "Kiema");
