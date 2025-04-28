d315b9e7f4f874d3ad2d129d8bf38ff4559ff8ad
mejja <majorkiema.mk@gmail.com>
2025-04-28 22:12:00 +0100

    02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest10.java

--- 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest10.java
+++ 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest10.java
@@ -3,22 +3,22 @@ import java.util.*;
 public class ArrayListTest10{
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<String>(); 
-        // Adding elements to object of List interface Custom inputs 
-        names.add(0, "Major"); 
+        List<String> names = new ArrayList<String>();
+        // Adding elements to object of List interface Custom inputs
+        names.add(0, "Major");
         names.add(1, "Mbandi");
-        names.add(2, "Amy"); 
-        names.add(3, "Kiema"); 
+        names.add(2, "Amy");
+        names.add(3, "Kiema");
 
-        ListIterator<String> itr=names.listIterator();    
-        System.out.println("Traversing elements in forward direction");  
-        while(itr.hasNext()){         
-        System.out.println("index:"+itr.nextIndex()+" value:"+itr.next());    
-        } 
+        ListIterator<String> itr=names.listIterator();
+        System.out.println("Traversing elements in forward direction");
+        while(itr.hasNext()){
+        System.out.println("index:"+itr.nextIndex()+" value:"+itr.next());
+        }
 
-        System.out.println("Traversing elements in backward direction"); 
-        while(itr.hasPrevious()){    
-        System.out.println("index:"+itr.previousIndex()+" value:"+itr.previous());    
-        }    
+        System.out.println("Traversing elements in backward direction");
+        while(itr.hasPrevious()){
+        System.out.println("index:"+itr.previousIndex()+" value:"+itr.previous());
+        }
     }
 }
