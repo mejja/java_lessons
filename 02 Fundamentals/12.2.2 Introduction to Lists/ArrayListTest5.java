bd8cd92e93a25a5622a4592cf06d867061788ec2
mejja <majorkiema.mk@gmail.com>
2025-05-09 22:07:11 +0100

    02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest5.java

--- 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest5.java
+++ 02 Fundamentals/12.2.2 Introduction to Lists/ArrayListTest5.java
@@ -3,14 +3,15 @@ import java.util.*;
 public class ArrayListTest5{
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<String>(); 
-        // Adding elements to object of List interface Custom inputs 
-        names.add(0, "Major"); 
+        List<String> names = new ArrayList<String>();
+        // Adding elements to object of List interface Custom inputs
+        names.add(0, "Major");
         names.add(1, "Mbandi");
-        names.add(2, "Amy"); 
-        names.add(3, "Kiema"); 
+        names.add(2, "Amy");
+        names.add(3, "Kiema");
+        names.add(4, "Kitonga");
         //List before clearance
-        System.out.println(names);  
+        System.out.println(names);
         //Get the size of the List
         System.out.println(names.size());
     }
