df712ef1e3898630484138b50d82f76c4e7912ba
mejja <majorkiema.mk@gmail.com>
2025-05-11 23:48:57 +0100

    expanded the list items to add

--- 02 Fundamentals/12.2.3  ArrayList/ArrayListTest.java
+++ 02 Fundamentals/12.2.3  ArrayList/ArrayListTest.java
@@ -4,10 +4,10 @@ public class ArrayListTest {
     public static void main(String[] args) {
 
         ArrayList<String> list = new ArrayList<>();
-        
         list.add("banana");
-           
+        list.add("apple");
+        list.add("orange");
             System.out.println(list);
-        
+
     }
 }
