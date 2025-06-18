512af61642db6e662b739bee1db2e76918bc7d93
mejja <majorkiema.mk@gmail.com>
2025-06-18 08:12:35 +0100

    Added 06.2 java.util/107.3 (Equals)/EquivalencyTest1.java

---
+++ 06.2 java.util/107.3 (Equals)/EquivalencyTest1.java
@@ -0,0 +1,11 @@
+public class EquivalencyTest1 {
+	public static void main(String[] args) {
+		
+		String a = "a";
+		
+		boolean result = a.equals(a);
+		
+		System.out.println("Result = "+result);
+		
+	}
+}
