6f6495d5e1f2884816a6f6f99094905c0fd40276
mejja <majorkiema.mk@gmail.com>
2025-06-19 08:13:50 +0100

    Added: 06.2 java.util/107.3 (Equals)/CompEg2b.java

---
+++ 06.2 java.util/107.3 (Equals)/CompEg2b.java
@@ -0,0 +1,10 @@
+public class CompEg2b {
+	public static void main(String[] args) {
+	    String a = "1";
+	    String b = "1";
+		
+		boolean result = a.equals(b);
+		
+		System.out.println("result = "+result);
+	}
+}
