456cdcd700219adc34127e5f7043fcafc951a7d3
mejja <majorkiema.mk@gmail.com>
2025-06-21 16:55:21 +0100

    Added: 06.2 java.util/107.3 (Equals)/CompEg3b.java

---
+++ 06.2 java.util/107.3 (Equals)/CompEg3b.java
@@ -0,0 +1,10 @@
+public class CompEg3b {
+	public static void main(String[] args) {
+	    String a = new String("1");
+	    String b = new String("1");
+		
+		boolean result = a.equals(b);
+		
+		System.out.println("result = "+result);
+	}
+}
