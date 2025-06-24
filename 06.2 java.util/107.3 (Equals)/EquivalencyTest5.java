75cfb70fa3822de028f0d30a32ff86047ba2bc55
mejja <majorkiema.mk@gmail.com>
2025-06-24 21:49:51 +0100

    Added: 06.2 java.util/107.3 (Equals)/EquivalencyTest5.java

---
+++ 06.2 java.util/107.3 (Equals)/EquivalencyTest5.java
@@ -0,0 +1,11 @@
+public class EquivalencyTest5 {
+	public static void main(String[] args) {
+		
+		String a = new String("a");
+		
+		boolean result = a.equals(null);
+		
+		System.out.println("Result1 = "+result);
+		
+	}
+}
