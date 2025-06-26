9cee5d57b2b424ffb942b7288af77035e3d4ce4b
mejja <majorkiema.mk@gmail.com>
2025-06-26 21:12:16 +0100

    06.2 java.util/107.3 (Equals)/EquivalencyTest4.java

---
+++ 06.2 java.util/107.3 (Equals)/EquivalencyTest4.java
@@ -0,0 +1,17 @@
+public class EquivalencyTest4 {
+	public static void main(String[] args) {
+		String a = new String("a");
+		String b = new String("a");
+		
+		boolean result = a.equals(b);
+		
+		System.out.println("Result1 = "+result);
+		
+		a.concat("heelo");
+		
+		result = a.equals(b);
+		
+		System.out.println("Result1 after concat = "+result);
+		
+	}
+}
