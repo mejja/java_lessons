ce0f412c4b4c87a36d03f80f3a88cf0130caba87
mejja <majorkiema.mk@gmail.com>
2025-06-28 23:13:00 +0100

    06.2 java.util/107.3 (Equals)/SampleEq.java

---
+++ 06.2 java.util/107.3 (Equals)/SampleEq.java
@@ -0,0 +1,25 @@
+public class SampleEq{
+
+    @Override
+    public boolean equals(Object param){
+       
+        //--check if it is null
+        if(param == null){
+            return false;
+        }
+
+        //--Check of it is the correct class
+        if(!(param instanceof SampleEq)){
+            return false;
+        }
+
+        //--Check if this is myself
+        if(this == param){
+            return true;
+        }
+        
+        //---Check for equality using fields (AKA instance variable) of this class
+        //---compared to the param
+        //---return true or false
+    }
+}
