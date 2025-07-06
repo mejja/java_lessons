3f6fbecaaa43af2775a39eddab4732bba4fbd012
mejja <majorkiema.mk@gmail.com>
2025-07-06 21:32:20 +0100

    06.2 java.util/103.3 (Set - NavigableSet)/

---
+++ 06.2 java.util/103.3 (Set - NavigableSet)/NavigableList1.java
@@ -0,0 +1,8 @@
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class NavigableList1{
+	public static void main(String[] args){
+		NavigableSet<Integer> navigableSet = new TreeSet<>();
+	}
+}
