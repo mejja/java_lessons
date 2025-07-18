97e7382ac9697f659508efff39d7fcb58995ded5
mejja <majorkiema.mk@gmail.com>
2025-07-18 07:58:34 +0100

    103.2 (Set - Hashset)/TestHashSetIsEmpty.java

---
+++ 103.2 (Set - Hashset)/TestHashSetIsEmpty.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestHashSetIsEmpty {
+    public static void main(String[] args) {
+    	// Create an instance of HashSet
+        Set<String> hashSet = new HashSet<>();
+
+        // Add Strings to the HashSet
+        hashSet.add("Java");
+        hashSet.add("Language");
+        System.out.println("HashSet: "+hashSet);
+
+        System.out.println("isEmpty:"+hashSet.isEmpty());
+    }
+}
