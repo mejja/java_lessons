b4d794044ee6484f52c05db233f03a43c3ae23db
mejja <majorkiema.mk@gmail.com>
2025-07-28 20:38:36 +0100

    06.2 java.util/103.4 (Set - Advanced)/TestVectorAdd.java

---
+++ 06.2 java.util/103.4 (Set - Advanced)/TestVectorAdd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TestVectorAdd{
+	public static void main(String[] args){
+		Vector <Long> stack = new Vector<>();
+
+		stack.add(303L);
+		stack.add(101L);
+		stack.add(202L);
+
+		System.out.println("The Vector Is: "+ stack);
+	}
+}
