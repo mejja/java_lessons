2815b45361c997c67137cc28a720f90726ff523d
mejja <majorkiema.mk@gmail.com>
2025-08-02 17:50:08 +0100

    06.2 java.util/105.2 (Queue - ConcurrentLinkedQueue)/

---
+++ 06.2 java.util/105.2 (Queue - ConcurrentLinkedQueue)/TestAdd.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TestAdd{
+	public static void main(String[] args){
+		//create a ConcurrentLinkedQueue instance
+		ConcurrentLinkedQueue<String> countries = new ConcurrentLinkedQueue<>();
+		//add Items
+		countries.add("Kenya");
+		countries.add("Tanzania");
+		countries.add("Uganda");
+		countries.add("Ethiopia");
+		countries.add("Somalia");
+		countries.add("South sudan");
+		countries.add("Rwanda");
+		countries.add("Burundi");
+		countries.add("South Africa");
+		countries.add("Nigeria");
+		//Print items in the list
+		System.out.println(countries);
+	}
+}
