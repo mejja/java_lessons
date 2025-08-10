1089e8032c4d59a07d8ccda1317cd7156b770e8d
mejja <majorkiema.mk@gmail.com>
2025-08-10 21:32:19 +0100

    06.2 java.util/107.4 (HashCode)/EquivalencyTest4.java

---
+++ 06.2 java.util/107.4 (HashCode)/EquivalencyTest4.java
@@ -0,0 +1,18 @@
+public class EquivalencyTest4 {
+	public static void main(String[] args) {
+		
+		String a = new String("a");
+		String b = new String("a");
+		
+		boolean result = a.equals(b);
+		
+		System.out.println("Result1 = "+result);
+		
+		a.concat("heelo");
+		
+		result = a.equals(b);
+		
+		System.out.println("Result1 after concat = "+result);
+		
+	}
+}
