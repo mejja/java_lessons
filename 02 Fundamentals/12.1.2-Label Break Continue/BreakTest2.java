8ec0d7c5f894c64dedff4734c3f0f225dba84f2d
mejja <majorkiema.mk@gmail.com>
2025-09-06 05:23:47 +0100

    02 Fundamentals/12.1.2-Label Break Continue/BreakTest2.java

--- 02 Fundamentals/12.1.2-Label Break Continue/BreakTest2.java
+++ 02 Fundamentals/12.1.2-Label Break Continue/BreakTest2.java
@@ -1,10 +1,12 @@
 public class BreakTest2 {
     public static void main(String[] args) {
-        int limit = 8;
-        int i=0;
-        while(i < limit){
-            System.out.println("Counter ="+i);
-            i++;
-            }
+        int limit = 8;           // Upper bound for the counter
+        int counter = 0;         // Initialize counter to 0
+
+        // Loop from 0 up to (but not including) the limit
+        while (counter < limit) {
+            System.out.println("Counter = " + counter); // Print current counter value
+            counter++; // Increment counter
+        }
     }
 }
