1411306770be48d6bf73a2fffd759ffea2df5366
mejja <majorkiema.mk@gmail.com>
2025-09-07 17:02:14 +0100

    02 Fundamentals/12.1.2-Label Break Continue/BlockTest2.java

--- 02 Fundamentals/12.1.2-Label Break Continue/BlockTest2.java
+++ 02 Fundamentals/12.1.2-Label Break Continue/BlockTest2.java
@@ -1,12 +1,16 @@
 public class BlockTest2 {
     public static void main(String[] args) {
-        label1:{
-           int i =1;
-           System.out.println("Block 1 i = "+ i);
+
+        // Labeled block: label1
+        label1: {
+            int i = 1; // Local variable scoped to label1 block
+            System.out.println("Block 1 i = " + i);
         }
-        label2:{
-            int i =1;
-            System.out.println("Block 2 i = "+ i);
+
+        // Labeled block: label2
+        label2: {
+            int i = 1; // Separate local variable scoped to label2 block
+            System.out.println("Block 2 i = " + i);
         }
     }
 }
