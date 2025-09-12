0005d96107523250668342a03ed5a7868d415864
mejja <majorkiema.mk@gmail.com>
2025-09-12 20:26:00 +0100

    03 Fundamentals/14 DeepDive 2 Arrays Basics/Arrays2Basics5.java

--- 03 Fundamentals/14 DeepDive 2 Arrays Basics/Arrays2Basics5.java
+++ 03 Fundamentals/14 DeepDive 2 Arrays Basics/Arrays2Basics5.java
@@ -1,17 +1,29 @@
-public class Arrays2Basics5{
-	public static void main(String[] args){
-		int[] arr = new int [50];
-		for(int i=0; i< arr.length; i++){
-			arr[i]= i+1;
-			if((arr[i] % 2 == 0) &&(arr[i] % 11 == 0))
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println(" ");
-		System.out.println("Array Values + 1");
-		for(int j=0; j< arr.length; j++){
-			int increment= arr[j]+1;
-			if((increment % 2==0) && (increment % 11 == 0))
-			System.out.print(increment + " ");
-		}
-	}
+public class Arrays2Basics5 {
+    public static void main(String[] args) {
+        // Initialize an array of size 50
+        int[] numbers = new int[50];
+
+        // Populate the array with values from 1 to 50
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
+
+            // Print values divisible by both 2 and 11
+            if (numbers[i] % 2 == 0 && numbers[i] % 11 == 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+
+        System.out.println(); // Line break for clarity
+
+        System.out.println("Array values incremented by 1 that are divisible by both 2 and 11:");
+
+        // Check incremented values for divisibility by 2 and 11
+        for (int j = 0; j < numbers.length; j++) {
+            int incrementedValue = numbers[j] + 1;
+
+            if (incrementedValue % 2 == 0 && incrementedValue % 11 == 0) {
+                System.out.print(incrementedValue + " ");
+            }
+        }
+    }
 }
