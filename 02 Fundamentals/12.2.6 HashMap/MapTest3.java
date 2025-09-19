006eb21a77e7e4fc78ef4a4683f73f4a7910e176
mejja <majorkiema.mk@gmail.com>
2025-09-19 09:13:00 +0100

    02 Fundamentals/12.2.6 HashMap/MapTest3.java

--- 02 Fundamentals/12.2.6 HashMap/MapTest3.java
+++ 02 Fundamentals/12.2.6 HashMap/MapTest3.java
@@ -3,22 +3,28 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class MapTest3 {
-	public static void main(String[] args) {
-		HashMap<String, String> hashMap = new HashMap<>();
-		
-		hashMap.put("key1", "A");
-		hashMap.put("key2", "B");
-		hashMap.put("key3", "C");
-		hashMap.put("key4", "D");
-		hashMap.put("key5", "E");
-		hashMap.put("key6", "F");
-		
-		Set<String> keySet = hashMap.keySet();		
-		Collection<String> values = hashMap.values();
+    public static void main(String[] args) {
+        // Create a HashMap to store key-value pairs
+        HashMap<String, String> hashMap = new HashMap<>();
 
-		 // Iterate over the keys and print them
+        // Populate the HashMap with sample entries
+        hashMap.put("key1", "A");
+        hashMap.put("key2", "B");
+        hashMap.put("key3", "C");
+        hashMap.put("key4", "D");
+        hashMap.put("key5", "E");
+        hashMap.put("key6", "F");
+
+        // Retrieve the set of keys (not used in this version, but available)
+        Set<String> keySet = hashMap.keySet();
+
+        // Retrieve the collection of values
+        Collection<String> values = hashMap.values();
+
+        // Print all values in the HashMap
+        System.out.println("Values in the HashMap:");
         for (String value : values) {
             System.out.println(value);
-        } 		
-	}	
+        }
+    }
 }
