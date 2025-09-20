e2f8338c76228f381d3186e3e37ee91bbbae9e5e
mejja <majorkiema.mk@gmail.com>
2025-09-20 16:56:06 +0100

    02 Fundamentals/12.2.6 HashMap/MapTest4.java

--- 02 Fundamentals/12.2.6 HashMap/MapTest4.java
+++ 02 Fundamentals/12.2.6 HashMap/MapTest4.java
@@ -1,26 +1,34 @@
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.Map.Entry; 
+import java.util.Map.Entry;
 
 public class MapTest4 {
-	public static void main(String[] args) {
-		HashMap<String, String> hashMap = new HashMap<>();
-		
-		hashMap.put("12", "Number 1");
-		hashMap.put("13", "Number 2");
-		hashMap.put("24", "Number 3");
-		hashMap.put("33", "Number 4");
-		hashMap.put("rr", "Number 5");
-		hashMap.put("gt", "Number 6");
-		hashMap.put("qq", "Number 7");
-		
-		Set<String> keySet = hashMap.keySet();		
-		Collection<String> values = hashMap.values();
+    public static void main(String[] args) {
+        // Create a HashMap to store key-value pairs
+        HashMap<String, String> hashMap = new HashMap<>();
 
-		 // Iterate over the entries and print key-value pairs
+        // Populate the HashMap with sample entries
+        hashMap.put("12", "Number 1");
+        hashMap.put("13", "Number 2");
+        hashMap.put("24", "Number 3");
+        hashMap.put("33", "Number 4");
+        hashMap.put("rr", "Number 5");
+        hashMap.put("gt", "Number 6");
+        hashMap.put("qq", "Number 7");
+
+        // Retrieve the set of keys (not used here but available)
+        Set<String> keySet = hashMap.keySet();
+
+        // Retrieve the collection of values (also unused here)
+        Collection<String> values = hashMap.values();
+
+        // Iterate over the entries and print key-value pairs
+        System.out.println("HashMap Entries:");
         for (Entry<String, String> entry : hashMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        } 		
-	}	
+            String key = entry.getKey();       // Get the key
+            String value = entry.getValue();   // Get the value
+            System.out.println(key + ": " + value); // Print key-value pair
+        }
+    }
 }
