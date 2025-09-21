f15e0346533cff9339a0a2a6085afc6aca2efdf0
mejja <majorkiema.mk@gmail.com>
2025-09-21 07:04:51 +0100

    02 Fundamentals/12.2.6 HashMap/MapTest5.java

--- 02 Fundamentals/12.2.6 HashMap/MapTest5.java
+++ 02 Fundamentals/12.2.6 HashMap/MapTest5.java
@@ -1,25 +1,33 @@
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.Map.Entry; 
+import java.util.Map.Entry;
 
 public class MapTest5 {
-	public static void main(String[] args) {
-		HashMap<String, String> hashMap = new HashMap<>();
-		
-		hashMap.put("Major",     "Mbandi");
-		hashMap.put("Amy",       "Kiema");
-		hashMap.put("Chief",     "Kiema");
-		hashMap.put("Mitchelle", "Kiema");
-		hashMap.put("Florence",  "Beti");
-		hashMap.put("Kiema",     "Mbandi");
-		
-		Set<String> keySet = hashMap.keySet();		
-		Collection<String> values = hashMap.values();
+    public static void main(String[] args) {
+        // Create a HashMap to store first name as key and surname as value
+        HashMap<String, String> hashMap = new HashMap<>();
 
-		 // Iterate over the entries and print key-value pairs
+        // Populate the HashMap with name pairs
+        hashMap.put("Major",     "Mbandi");
+        hashMap.put("Amy",       "Kiema");
+        hashMap.put("Chief",     "Kiema");
+        hashMap.put("Mitchelle", "Kiema");
+        hashMap.put("Florence",  "Beti");
+        hashMap.put("Kiema",     "Mbandi"); // 'Kiema' used as a key here
+
+        // Retrieve the set of keys (not used directly here)
+        Set<String> keySet = hashMap.keySet();
+
+        // Retrieve the collection of values (also unused directly)
+        Collection<String> values = hashMap.values();
+
+        // Print all key-value pairs in the HashMap
+        System.out.println("HashMap Entries:");
         for (Entry<String, String> entry : hashMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        } 		
-	}	
+            String key = entry.getKey();       // Get the key (first name)
+            String value = entry.getValue();   // Get the value (surname)
+            System.out.println(key + ": " + value); // Print formatted output
+        }
+    }
 }
