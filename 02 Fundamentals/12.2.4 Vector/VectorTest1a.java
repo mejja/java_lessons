969c8b76185f0b7c068eef1bf72e4dcc5ab2702f
mejja <majorkiema.mk@gmail.com>
2025-09-27 13:54:01 +0100

    02 Fundamentals/12.2.4 Vector/VectorTest1a.java

--- 02 Fundamentals/12.2.4 Vector/VectorTest1a.java
+++ 02 Fundamentals/12.2.4 Vector/VectorTest1a.java
@@ -1,19 +1,21 @@
-import java.util.*;
+import java.util.Vector;
 
 public class VectorTest1a {
     public static void main(String[] args) {
 
-        Vector<String> v = new Vector<>();
-        
-        v.add("banana");
-        v.add("apples");
-        v.add("Pineapples");
-        v.add("mangoes");
-        v.add("oranges");
-        v.add("watermelon");
-        v.add("grapes");
-           
-        System.out.println(v);
-        
+        // Create a Vector to store fruit names
+        Vector<String> fruits = new Vector<>();
+
+        // Add various fruits to the vector
+        fruits.add("banana");
+        fruits.add("apples");
+        fruits.add("Pineapples");
+        fruits.add("mangoes");
+        fruits.add("oranges");
+        fruits.add("watermelon");
+        fruits.add("grapes");
+
+        // Display the contents of the vector
+        System.out.println("Available fruits: " + fruits);
     }
 }
