8ec144a537b16a8a0fee5109877fc94dc6d73678
mejja <majorkiema.mk@gmail.com>
2025-10-03 21:43:05 +0100

    02 Fundamentals/12.2.3  ArrayList/ArrayListTest3.java

--- 02 Fundamentals/12.2.3  ArrayList/ArrayListTest3.java
+++ 02 Fundamentals/12.2.3  ArrayList/ArrayListTest3.java
@@ -1,19 +1,23 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class ArrayListTest3 {
     public static void main(String[] args) {
 
-        ArrayList<String> name = new ArrayList<>();
-        
-        name.add("Major");
-        name.add("Kiema");
-        name.add("Mbandi");
-           
-            System.out.println(name);
+        // Create an ArrayList to store names
+        ArrayList<String> names = new ArrayList<>();
 
-        name.remove(1);
+        // Add names to the list
+        names.add("Major");
+        names.add("Kiema");
+        names.add("Mbandi");
 
-            System.out.println(name);
-        
+        // Display the original list of names
+        System.out.println("Original list: " + names);
+
+        // Remove the second element ("Kiema") from the list
+        names.remove(1); // Index 1 corresponds to "Kiema"
+
+        // Display the updated list after removal
+        System.out.println("Updated list (after removing 'Kiema'): " + names);
     }
 }
