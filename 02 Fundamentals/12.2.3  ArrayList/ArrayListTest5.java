323780b732c34a342b48ad0fa9c022647f132749
mejja <majorkiema.mk@gmail.com>
2025-10-05 20:52:38 +0100

    02 Fundamentals/12.2.3  ArrayList/ArrayListTest5.java

--- 02 Fundamentals/12.2.3  ArrayList/ArrayListTest5.java
+++ 02 Fundamentals/12.2.3  ArrayList/ArrayListTest5.java
@@ -1,16 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class ArrayListTest5 {
     public static void main(String[] args) {
 
-        ArrayList<String> name = new ArrayList<>();
-        
-        name.add("Major");
-        name.add("Kiema");
-        name.add("Mbandi");
-           
-            System.out.println(name);
-            System.out.println(name.size());
-        
+        // Create an ArrayList to store names
+        ArrayList<String> names = new ArrayList<>();
+
+        // Add names to the list
+        names.add("Major");
+        names.add("Kiema");
+        names.add("Mbandi");
+
+        // Display the list of names
+        System.out.println("Names in the list: " + names);
+
+        // Display the number of elements in the list
+        System.out.println("Total number of names: " + names.size());
     }
 }
