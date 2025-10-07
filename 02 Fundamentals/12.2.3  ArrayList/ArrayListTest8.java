d3793b336eb53308200007d7fa590ae9bbabd9d7
mejja <majorkiema.mk@gmail.com>
2025-10-07 18:35:13 +0100

    02 Fundamentals/12.2.3  ArrayList/ArrayListTest8.java

--- 02 Fundamentals/12.2.3  ArrayList/ArrayListTest8.java
+++ 02 Fundamentals/12.2.3  ArrayList/ArrayListTest8.java
@@ -6,12 +6,12 @@ public class ArrayListTest8 {
 
         //create an empty array myName with initial capacity as 5
         ArrayList<String> myName = new ArrayList<String>(5);
-        
+
         //adding element to list using add() method
         myName.add("Major");
         myName.add("Kiema");
         myName.add("Mbandi");
-        
+
         //prints all the elements available in the myName
         System.out.println("Printing myName:");
         for (String name : myName)
@@ -19,35 +19,34 @@ public class ArrayListTest8 {
 
         //create an empty array myName with initial capacity as 3
         ArrayList<String> sisterName = new ArrayList<String>(3);
-        
+
         //adding element to list using add() method
         sisterName.add("Amy");
         sisterName.add("Kiema");
-        
+
         //prints all the elements available in the myName
         System.out.println("Printing sisterName:");
         for (String name : sisterName)
             System.out.println("Name is: " + name);
 
-        // create an empty array list1 with initial capacity as 3 
-        ArrayList<String> brotherName =  new ArrayList<String>(3);   
-        // use add() method to add elements in the list 
-        brotherName.add("Chief"); 
-        brotherName.add("Kitonga"); 
-        brotherName.add("Kiema");   
-        // prints all the elements available in brotherName 
-        System.out.println("Printing list1:"); 
-        for (String name : brotherName)  
-        System.out.println("Name is: " + name); 
+        // create an empty array list1 with initial capacity as 3
+        ArrayList<String> brotherName =  new ArrayList<String>(3);
+        // use add() method to add elements in the list
+        brotherName.add("Chief");
+        brotherName.add("Kitonga");
+        brotherName.add("Kiema");
+        // prints all the elements available in brotherName
+        System.out.println("Printing list1:");
+        for (String name : brotherName)
+        System.out.println("Name is: " + name);
 
-        // inserting all elements, sisterName & list1 will get printed after myname 
-        myName.addAll(sisterName); 
+        // inserting all elements, sisterName & list1 will get printed after myname
+        myName.addAll(sisterName);
         myName.addAll(2, brotherName); //brother's inserted b4 sister's
 
-         System.out.println("Printing all the elements"); 
+         System.out.println("Printing all the elements");
         // let us print all the elements available in myName
-        for (String name : myName)  
+        for (String name : myName)
             System.out.println("Name is: " + name);
-        
     }
 }
