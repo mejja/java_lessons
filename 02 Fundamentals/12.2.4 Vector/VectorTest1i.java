ccc30d999a1a6817c06dce1b13b4747673c581a0
mejja <majorkiema.mk@gmail.com>
2025-10-11 06:18:13 +0100

    02 Fundamentals/12.2.4 Vector/VectorTest1i.java

--- 02 Fundamentals/12.2.4 Vector/VectorTest1i.java
+++ 02 Fundamentals/12.2.4 Vector/VectorTest1i.java
@@ -1,10 +1,13 @@
-import java.util.*;
+import java.util.Iterator;
+import java.util.Vector;
 
 public class VectorTest1i {
     public static void main(String[] args) {
 
+        // Create a Vector to store a phrase
         Vector<String> phrase = new Vector<>();
-        
+
+        // Add words to form a sentence
         phrase.add("I");
         phrase.add("am");
         phrase.add("learning");
@@ -12,14 +15,16 @@ public class VectorTest1i {
         phrase.add("with");
         phrase.add("OpenLearn");
 
-        System.out.println("Vector Phrase"+ phrase); 
-        
-        Iterator value =phrase.iterator();
+        // Display the full phrase as a vector
+        System.out.println("Vector contents: " + phrase);
 
-        System.out.println("The iterator values are: "); 
-        while(value.hasNext()){
-            System.out.println(value.next());
-        }       
+        // Use an iterator to traverse the vector
+        Iterator<String> iterator = phrase.iterator();
 
-    }        
+        System.out.println("Iterating through the vector:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
+
