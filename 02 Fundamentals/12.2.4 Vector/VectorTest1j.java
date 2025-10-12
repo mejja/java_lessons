6feae0392e6b5eded9398e228f6a9d429ef1545f
mejja <majorkiema.mk@gmail.com>
2025-10-12 20:35:07 +0100

    02 Fundamentals/12.2.4 Vector/VectorTest1j.java

--- 02 Fundamentals/12.2.4 Vector/VectorTest1j.java
+++ 02 Fundamentals/12.2.4 Vector/VectorTest1j.java
@@ -1,24 +1,29 @@
-import java.util.*;
+import java.util.Vector;
 
 public class VectorTest1j {
     public static void main(String[] args) {
 
+        // Create a Vector to store words forming a phrase
         Vector<String> phrase = new Vector<>();
-        
+
+        // Add words to the vector
         phrase.add("I");
         phrase.add("am");
         phrase.add("learning");
         phrase.add("Java");
         phrase.add("with");
         phrase.add("OpenLearn");
-        //print the vector elements
-        System.out.println("Vector Phrase"+ phrase); 
-        
+
+        // Display the vector contents
+        System.out.println("Vector contents: " + phrase);
+
+        // Convert the vector to an array
         Object[] arr = phrase.toArray();
 
-        System.out.println("The Array is: "); 
-        for(int j =0; j<arr.length; j++){
-            System.out.println(arr[j]);
-        }       
-    }        
+        // Display the array contents
+        System.out.println("Array contents:");
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println("Index " + j + ": " + arr[j]);
+        }
+    }
 }
