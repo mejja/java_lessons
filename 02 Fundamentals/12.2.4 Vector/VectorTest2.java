8fdb42b7485c55b01dcbc43a28e4aad66ee98a3b
mejja <majorkiema.mk@gmail.com>
2025-10-13 01:21:55 +0100

    02 Fundamentals/12.2.4 Vector/VectorTest2.java

--- 02 Fundamentals/12.2.4 Vector/VectorTest2.java
+++ 02 Fundamentals/12.2.4 Vector/VectorTest2.java
@@ -1,16 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VectorTest2 {
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<String>(); 
-        // Adding elements to object of List interface Custom inputs 
-        names.add(0, "Major"); 
+        // Create a List to store names using ArrayList implementation
+        List<String> names = new ArrayList<>();
+
+        // Add names to the list at specific indices
+        names.add(0, "Major");
         names.add(1, "Mbandi");
-        names.add(2, "Amy"); 
-        names.add(3, "Kiema"); 
-        
-        System.out.println(names);
-        
+        names.add(2, "Amy");
+        names.add(3, "Kiema");
+
+        // Display the list of names
+        System.out.println("Names list: " + names);
     }
 }
+
