568ee8978648f11f1467b59e8a7d030f218cf792
mejja <majorkiema.mk@gmail.com>
2025-10-14 17:22:41 +0300

    02 Fundamentals/12.2.4 Vector/VectorTest10.java

--- 02 Fundamentals/12.2.4 Vector/VectorTest10.java
+++ 02 Fundamentals/12.2.4 Vector/VectorTest10.java
@@ -1,24 +1,32 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class VectorTest10 {
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<String>(); 
-        // Adding elements to object of List interface Custom inputs 
-        names.add(0, "Major"); 
+        // Create a List to store names using ArrayList implementation
+        List<String> names = new ArrayList<>();
+
+        // Add names to the list at specific indices
+        names.add(0, "Major");
         names.add(1, "Mbandi");
-        names.add(2, "Amy"); 
-        names.add(3, "Kiema"); 
+        names.add(2, "Amy");
+        names.add(3, "Kiema");
 
-        ListIterator<String> itr=names.listIterator();    
-        System.out.println("Traversing elements in forward direction");  
-        while(itr.hasNext()){         
-        System.out.println("index:"+itr.nextIndex()+" value:"+itr.next());    
-        } 
+        // Create a ListIterator to traverse the list
+        ListIterator<String> iterator = names.listIterator();
 
-        System.out.println("Traversing elements in backward direction"); 
-        while(itr.hasPrevious()){    
-        System.out.println("index:"+itr.previousIndex()+" value:"+itr.previous());    
-        }    
+        // Traverse the list in forward direction
+        System.out.println("Traversing elements in forward direction:");
+        while (iterator.hasNext()) {
+            System.out.println("Index: " + iterator.nextIndex() + " | Value: " + iterator.next());
+        }
+
+        // Traverse the list in backward direction
+        System.out.println("Traversing elements in backward direction:");
+        while (iterator.hasPrevious()) {
+            System.out.println("Index: " + iterator.previousIndex() + " | Value: " + iterator.previous());
+        }
     }
 }
