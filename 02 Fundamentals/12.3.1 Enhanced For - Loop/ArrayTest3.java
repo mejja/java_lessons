9fdf8afbfdd902996372aa00d413ea8d0b995ad6
mejja <majorkiema.mk@gmail.com>
2025-10-15 10:03:23 +0100

    02 Fundamentals/12.3.1 Enhanced For - Loop/ArrayTest3.java

--- 02 Fundamentals/12.3.1 Enhanced For - Loop/ArrayTest3.java
+++ 02 Fundamentals/12.3.1 Enhanced For - Loop/ArrayTest3.java
@@ -1,12 +1,13 @@
 public class ArrayTest3 {
     public static void main(String[] args) {
-        int [] lottonumbers = {233,444,678,675,897,678,453,567,455,456,453};
-       
-        int i = 0;       
+        // Initialize an array with lotto numbers
+        int[] lottoNumbers = {233, 444, 678, 675, 897, 678, 453, 567, 455, 456, 453};
 
-        for(int tempValue : lottonumbers){
-            System.out.println("index = "+ i +" value = "+tempValue);
-            i++;
+        // Use a for-each loop with an external index to print each value
+        int index = 0;
+        for (int number : lottoNumbers) {
+            System.out.println("Index = " + index + " | Value = " + number);
+            index++;
         }
     }
 }
