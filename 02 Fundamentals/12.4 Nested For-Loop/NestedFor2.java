e534b57f9ded21b5e2dc55ebd61ebfb497fb4f25
mejja <majorkiema.mk@gmail.com>
2025-10-18 17:24:18 +0100

    02 Fundamentals/12.4 Nested For-Loop/NestedFor2.java

--- 02 Fundamentals/12.4 Nested For-Loop/NestedFor2.java
+++ 02 Fundamentals/12.4 Nested For-Loop/NestedFor2.java
@@ -1,9 +1,13 @@
 public class NestedFor2 {
-    public static void main(String[] args) {  
-        for(int i = 0;i<5;i++){
-            for(int j = 0;j<5;j++){
-                System.out.println("Outer = "+i +" Inner = "+j);
-            }           
+    public static void main(String[] args) {
+
+        // Outer loop runs from 0 to 4
+        for (int outer = 0; outer < 5; outer++) {
+
+            // Inner loop runs from 0 to 4 for each outer iteration
+            for (int inner = 0; inner < 5; inner++) {
+                System.out.println("Outer = " + outer + " | Inner = " + inner);
+            }
         }
     }
 }
