844052996015c638f9e5d0126d15aa065504958a
mejja <majorkiema.mk@gmail.com>
2025-10-23 08:04:54 +0100

    02 Fundamentals/12.2.4 Vector/VectorTest1e.java

--- 02 Fundamentals/12.2.4 Vector/VectorTest1e.java
+++ 02 Fundamentals/12.2.4 Vector/VectorTest1e.java
@@ -1,21 +1,24 @@
-import java.util.*;
+import java.util.Vector;
 
 public class VectorTest1e {
     public static void main(String[] args) {
 
-        Vector<String> v = new Vector<>();
-        
-        v.add("banana");
-        v.add("apples");
-        v.add("Pineapples");
-        v.add("mangoes");
-        v.add("oranges");
-        v.add("watermelon");
-        v.add("grapes");
-           
-        System.out.println(v);
+        // Create a Vector to store fruit names
+        Vector<String> fruits = new Vector<>();
 
-        System.out.println("check if v contains string grapes "); 
-        System.out.println(v.contains("grapes"));
+        // Add fruits to the vector
+        fruits.add("banana");
+        fruits.add("apples");
+        fruits.add("Pineapples");
+        fruits.add("mangoes");
+        fruits.add("oranges");
+        fruits.add("watermelon");
+        fruits.add("grapes");
+
+        // Display the contents of the vector
+        System.out.println("Fruit list: " + fruits);
+
+        // Check if the vector contains the string "grapes"
+        System.out.println("Does the list contain 'grapes'? " + fruits.contains("grapes")); // Expected: true
     }
 }
