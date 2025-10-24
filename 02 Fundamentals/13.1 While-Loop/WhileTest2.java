7f7a5c4bebd2f9db6af623048eef3fb1cfed75d6
mejja <majorkiema.mk@gmail.com>
2025-10-24 08:29:58 +0100

    02 Fundamentals/13.1 While-Loop/WhileTest2.java

--- 02 Fundamentals/13.1 While-Loop/WhileTest2.java
+++ 02 Fundamentals/13.1 While-Loop/WhileTest2.java
@@ -1,9 +1,14 @@
 public class WhileTest2 {
     public static void main(String[] args) {
-        int counter = 0;      
-        while(counter++ < 10){
-            if(counter%2== 0)
-            System.out.println("Counter = "+counter);
+        int counter = 0;
+
+        // Loop runs while counter is less than 10
+        while (counter++ < 10) {
+            // counter is incremented before the condition is checked
+            // So printed values start from 1 and go up to 10
+            if (counter % 2 == 0) {
+                System.out.println("Counter = " + counter); // Prints even numbers only
+            }
         }
     }
 }
