8e0496bc8ef196a78f5a01c1a50c0f4abcfadd75
mejja <majorkiema.mk@gmail.com>
2025-10-25 18:34:49 +0100

    02 Fundamentals/13.2 Do-While Loop/DoWhileTest1.java

--- 02 Fundamentals/13.2 Do-While Loop/DoWhileTest1.java
+++ 02 Fundamentals/13.2 Do-While Loop/DoWhileTest1.java
@@ -1,8 +1,10 @@
 public class DoWhileTest1 {
     public static void main(String[] args) {
-        int counter = 0;      
-        do{
-            System.out.println("Counter = "+counter);
-        }while(counter++ < 10);
+        int counter = 0;
+
+        // The do-while loop executes at least once before checking the condition
+        do {
+            System.out.println("Counter = " + counter);
+        } while (counter++ < 10); // Post-increment: prints from 0 to 10
     }
 }
