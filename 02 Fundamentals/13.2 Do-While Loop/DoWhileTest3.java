2820114e03769c3cd65927f7c7f6fcf48d1fa3f3
mejja <majorkiema.mk@gmail.com>
2025-10-27 05:22:18 +0000

    02 Fundamentals/13.2 Do-While Loop/DoWhileTest3.java

--- 02 Fundamentals/13.2 Do-While Loop/DoWhileTest3.java
+++ 02 Fundamentals/13.2 Do-While Loop/DoWhileTest3.java
@@ -1,10 +1,14 @@
 public class DoWhileTest3 {
     public static void main(String[] args) {
-        int [] lottonumbers = {233,444,678,675,897,678,453,567,455,456,453};
-        int i =0;
-        do{
-            System.out.println("index = "+ i +" value = "+lottonumbers[i]);
-            i++;
-        }while(i < lottonumbers.length);
+        // Initialize an array with lotto numbers
+        int[] lottoNumbers = {233, 444, 678, 675, 897, 678, 453, 567, 455, 456, 453};
+
+        int index = 0;
+
+        // Use a do-while loop to print each element with its index
+        do {
+            System.out.println("Index = " + index + " | Value = " + lottoNumbers[index]);
+            index++;
+        } while (index < lottoNumbers.length); // Loop until all elements are printed
     }
 }
