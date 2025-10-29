b55f6846e6c29c87263f4f16cb653b5792664536
mejja <majorkiema.mk@gmail.com>
2025-10-29 22:03:55 +0000

    02 Fundamentals/13.3.1 Multi-Dimensional Arrays/MultiTest1.java

--- 02 Fundamentals/13.3.1 Multi-Dimensional Arrays/MultiTest1.java
+++ 02 Fundamentals/13.3.1 Multi-Dimensional Arrays/MultiTest1.java
@@ -1,13 +1,20 @@
 public class MultiTest1 {
     public static void main(String[] args) {
-    	
-        int [][] lottogroups = {{1111,2222,3333,4444},{6666,7777,8888,9999},{11111,121212,131313}};
-    	
-        for(int i = 0;i < lottogroups.length ;i++){
-            System.out.println("Group :"+i);
-            for(int j = 0;j < lottogroups[i].length ;j++){
-                System.out.println("  Value "+j+" = "+lottogroups[i][j]);
-            }        	
+        // Initialize a 2D array representing lotto number groups
+        int[][] lottoGroups = {
+            {1111, 2222, 3333, 4444},
+            {6666, 7777, 8888, 9999},
+            {11111, 121212, 131313}
+        };
+
+        // Traverse each group (row)
+        for (int groupIndex = 0; groupIndex < lottoGroups.length; groupIndex++) {
+            System.out.println("Group: " + groupIndex);
+
+            // Traverse each value within the group (column)
+            for (int valueIndex = 0; valueIndex < lottoGroups[groupIndex].length; valueIndex++) {
+                System.out.println("  Value " + valueIndex + " = " + lottoGroups[groupIndex][valueIndex]);
+            }
         }
     }
 }
