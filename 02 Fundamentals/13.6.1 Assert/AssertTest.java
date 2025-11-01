5d81ee2afc30da65f7caf79f1a64a5196e700dc7
mejja <majorkiema.mk@gmail.com>
2025-11-01 03:53:31 +0000

    02 Fundamentals/13.6.1 Assert/AssertTest.java

--- 02 Fundamentals/13.6.1 Assert/AssertTest.java
+++ 02 Fundamentals/13.6.1 Assert/AssertTest.java
@@ -1,9 +1,10 @@
 public class AssertTest {
     public static void main(String[] args) {
-        boolean condtion = true;
-       
-        assert condtion : "expected true condition";
-       
+        boolean condition = true;
+
+    // Assertion: will throw AssertionError with message if condition is false
+        assert condition : "Expected true condition";
+
         System.out.println("Finished");
     }
 }
