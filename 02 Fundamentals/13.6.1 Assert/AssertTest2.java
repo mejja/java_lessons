fcc2d3df01c124e7418911fde31ab0138d2fd847
mejja <majorkiema.mk@gmail.com>
2025-11-03 13:39:35 +0000

    02 Fundamentals/13.6.1 Assert/AssertTest2.java

--- 02 Fundamentals/13.6.1 Assert/AssertTest2.java
+++ 02 Fundamentals/13.6.1 Assert/AssertTest2.java
@@ -2,8 +2,13 @@ public class AssertTest2 {
     public static void main(String[] args) {
         int a = 1;
         int b = 4;
-        assert a>0;
-        assert b>10;
+
+        // This assertion passes since a > 0
+        assert a > 0 : "Assertion failed: a should be greater than 0";
+
+        // This assertion fails since b is not greater than 10
+        assert b > 10 : "Assertion failed: b should be greater than 10";
+
         System.out.println("Finished");
     }
 }
