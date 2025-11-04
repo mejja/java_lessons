1ea591e0b2e55ca8b60803c81a05601f9cb6cf91
mejja <majorkiema.mk@gmail.com>
2025-11-04 14:29:04 +0000

    03 Fundamentals/14 DeepDive 3 Going Bracketless/Bracketless1.java

--- 03 Fundamentals/14 DeepDive 3 Going Bracketless/Bracketless1.java
+++ 03 Fundamentals/14 DeepDive 3 Going Bracketless/Bracketless1.java
@@ -1,7 +1,9 @@
-public class Bracketless1{
-	public static void main(String[] args){
-		for(int i=0;i<10;i++)
-			if(i>=1)
-			System.out.println("Greater than "+ i);
-	}
+public class Bracketless1 {
+    public static void main(String[] args) {
+        // Loop from 0 to 9
+        for (int i = 0; i < 10; i++)
+            // Only print if i is greater than or equal to 1
+            if (i >= 1)
+                System.out.println("Greater than " + i);
+    }
 }
