6311fe8d6206ccc9fbaa98aa6009b0a0de0e66e6
mejja <majorkiema.mk@gmail.com>
2025-11-05 07:16:29 +0000

    03 Fundamentals/14 DeepDive 3 Going Bracketless/Bracketless2.java

--- 03 Fundamentals/14 DeepDive 3 Going Bracketless/Bracketless2.java
+++ 03 Fundamentals/14 DeepDive 3 Going Bracketless/Bracketless2.java
@@ -1,9 +1,15 @@
-public class Bracketless2{
-	public static void main(String[] args){
-		int i=0;
-		while(i<10){
-			if(i>1)
-			System.out.println("Greater than "+ i);
-			i++;}
-	}
+public class Bracketless2 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        // While loop runs while i < 10
+        while (i < 10) {
+            // If i > 1, print the message
+            if (i > 1)
+                System.out.println("Greater than " + i);
+
+            // Increment i after the if block
+            i++;
+        }
+    }
 }
