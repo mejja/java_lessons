e4d8b6ccd985174cb1d1ac93f960d6bf2d68eab6
mejja <majorkiema.mk@gmail.com>
2025-11-07 19:51:18 +0000

    03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation1.java

--- 03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation1.java
+++ 03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation1.java
@@ -1,23 +1,26 @@
 public class ArrayPopulation1 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+        // Create an integer array of size 10 (default values are 0)
         int[] array = new int[10];
-        printArray("array: ",array);
 
+        // Print the array using a custom method
+        printArray("array: ", array);
     }
-    
-    public static void printArray(String prefix,int[] arrayToPrint){
+
+    // Method to print array contents with a prefix
+    public static void printArray(String prefix, int[] arrayToPrint) {
         System.out.print(prefix);
         System.out.print("[");
-    
-        for (int i =0;i < arrayToPrint.length;i++) {
-            
-            System.out.print(arrayToPrint[i]);
-            if(i != arrayToPrint.length-1){
-            	System.out.print(",");
-            }
 
-            
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.print(arrayToPrint[i]);
+
+            // Add comma between elements except after the last one
+            if (i != arrayToPrint.length - 1) {
+                System.out.print(", ");
+            }
         }
-        System.out.print("]\n");
+
+        System.out.println("]");
     }
 }
