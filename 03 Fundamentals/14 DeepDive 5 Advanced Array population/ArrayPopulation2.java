9e3ad1ad643daeaf464b7e3ae81d4f42e2710175
mejja <majorkiema.mk@gmail.com>
2025-11-09 18:30:53 +0000

    03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation2.java

--- 03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation2.java
+++ 03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation2.java
@@ -1,26 +1,36 @@
 public class ArrayPopulation2 {
-    public static void main(String[]args){
-        int[] array = new int[10];
-        
-         array[4]=-1;
 
-        printArray("array: ",array);
+    public static void main(String[] args) {
+        // Create an integer array of size 10
+        int[] numbers = new int[10];  // All elements default to 0
+
+        // Set the value at index 4 to -1
+        numbers[4] = -1;
+
+        // Print the contents of the array with a label
+        printArray("Array: ", numbers);
     }
 
-    public static void printArray(String prefix,int[] arrayToPrint){
-        System.out.print(prefix);
+    /**
+     * Prints the contents of an integer array in a formatted way.
+     * Example output: Array: [0, 0, 0, 0, -1, 0, 0, 0, 0, 0]
+     *
+     * @param label         A prefix label to print before the array
+     * @param arrayToPrint  The integer array to display
+     */
+    public static void printArray(String label, int[] arrayToPrint) {
+        System.out.print(label);
         System.out.print("[");
-    
-        for (int i =0;i < arrayToPrint.length;i++) {
-            
+
+        for (int i = 0; i < arrayToPrint.length; i++) {
             System.out.print(arrayToPrint[i]);
-            if(i != arrayToPrint.length-1){
-            	System.out.print(",");
+
+            // Add a comma after each element except the last
+            if (i != arrayToPrint.length - 1) {
+                System.out.print(", ");
             }
-
-            
         }
-        System.out.print("]\n");
-    }
 
+        System.out.println("]");
+    }
 }
