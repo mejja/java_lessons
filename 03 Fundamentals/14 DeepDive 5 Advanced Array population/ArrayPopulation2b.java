8b37589ff47d17f9f0d4c3f152967b8ce16aead9
mejja <majorkiema.mk@gmail.com>
2025-11-10 20:43:49 +0000

    03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation2b.java

--- 03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation2b.java
+++ 03 Fundamentals/14 DeepDive 5 Advanced Array population/ArrayPopulation2b.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class ArrayPopulation1b {
+
     public static void main(String[] args) {
+        // Initialize arrays of various primitive types and String
         boolean[] arrayBol   = new boolean[10];
         byte[] arrayByte     = new byte[10];
         short[] arrayShort   = new short[10];
@@ -10,18 +14,18 @@ public class ArrayPopulation1b {
         double[] arrayDouble = new double[10];
         String[] arrayString = new String[10];
 
-        //Assigning values
-        arrayBol[9]     =true;
-        arrayByte[9]    =(byte)127;
-        arrayShort[9]   =50;
-        arrayChar[9]    ='z';
-        arrayInt[9]     =99;
-        arrayFloat[9]   =100.99f;
-        arrayLong[9]    =200;
-        arrayDouble[9]  =200.99;
+        // Assign values to the last index of each array
+        arrayBol[9]     = true;
+        arrayByte[9]    = (byte)127;
+        arrayShort[9]   = 50;
+        arrayChar[9]    = 'z';
+        arrayInt[9]     = 99;
+        arrayFloat[9]   = 100.99f;
+        arrayLong[9]    = 200L;
+        arrayDouble[9]  = 200.99;
         arrayString[9]  = "final";
 
-        // Default Values printing
+        // Print default and assigned values of each array
         System.out.println("Printing Default Arrays values:");
         printArray("Boolean: ", arrayBol);
         printArray("Byte: ", arrayByte);
@@ -34,111 +38,48 @@ public class ArrayPopulation1b {
         printArray("String: ", arrayString);
     }
 
-    public static void printArray(String prefix, boolean[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for boolean arrays
+    public static void printArray(String prefix, boolean[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, byte[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for byte arrays
+    public static void printArray(String prefix, byte[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, short[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for short arrays
+    public static void printArray(String prefix, short[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, char[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for char arrays
+    public static void printArray(String prefix, char[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, int[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for int arrays
+    public static void printArray(String prefix, int[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, float[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for float arrays
+    public static void printArray(String prefix, float[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, long[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for long arrays
+    public static void printArray(String prefix, long[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, double[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for double arrays
+    public static void printArray(String prefix, double[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 
-    public static void printArray(String prefix, String[] arrayToPrint) {
-        System.out.print(prefix);
-        System.out.print("[");
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(arrayToPrint[i]);
-            if (i != arrayToPrint.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("]\n");
+    // Generic print method for String arrays
+    public static void printArray(String prefix, String[] array) {
+        System.out.println(prefix + Arrays.toString(array));
     }
 }
