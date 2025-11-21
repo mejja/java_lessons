ad16f7358eec1886179a8eb8cee659ca03e4b28d
mejja <majorkiema.mk@gmail.com>
2025-11-21 19:21:27 +0000

    04 (LocalTime)/LocalDateTest.java

--- 06.2 java.util/04 (LocalTime)/LocalDateTest.java
+++ 06.2 java.util/04 (LocalTime)/LocalDateTest.java
@@ -1,9 +1,15 @@
 import java.time.LocalTime;
 
+/**
+ * Demonstrates how to get the current time using LocalTime in Java
+ */
 public class LocalDateTest {
+
     public static void main(String[] args) {
-   		 LocalTime currentTime =LocalTime.now();   		 
-   		 System.out.println("today’s Date is : "+ currentTime) ;
-   		 
+        // Get the current local time
+        LocalTime currentTime = LocalTime.now();
+
+        // Print the current time
+        System.out.println("Current Time is: " + currentTime);
     }
 }
