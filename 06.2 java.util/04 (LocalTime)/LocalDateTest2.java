6feb791c7be8524c16a394aaaacc27c2564edc4e
mejja <majorkiema.mk@gmail.com>
2025-11-22 19:23:04 +0000

    04 (LocalTime)/LocalDateTest2.java

--- 06.2 java.util/04 (LocalTime)/LocalDateTest2.java
+++ 06.2 java.util/04 (LocalTime)/LocalDateTest2.java
@@ -1,12 +1,21 @@
 import java.time.LocalTime;
 
+/**
+ * Demonstrates how to get the current time and its components using LocalTime in Java
+ */
 public class LocalDateTest2 {
+
     public static void main(String[] args) {
-   		 LocalTime currentTime =LocalTime.now();
-         int currentHour = currentTime.getHour();
-         int currentMinutes = currentTime.getMinute();  		 
-   		 System.out.println("Today’s Date is : "+ currentTime);
-         System.out.println("Current Hour is : "+ currentHour);
-         System.out.println("Current Minutes is : "+ currentMinutes); 
+        // Get the current local time
+        LocalTime currentTime = LocalTime.now();
+
+        // Extract the current hour and minute
+        int currentHour = currentTime.getHour();
+        int currentMinute = currentTime.getMinute();
+
+        // Print the current time and its components
+        System.out.println("Current Time is: " + currentTime);
+        System.out.println("Current Hour is: " + currentHour);
+        System.out.println("Current Minute is: " + currentMinute);
     }
 }
