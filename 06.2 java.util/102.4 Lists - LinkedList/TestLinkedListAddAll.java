256d75ed3a0c80ede68a9dd706cf54c87fa17c9f
mejja <majorkiema.mk@gmail.com>
2025-11-26 19:26:01 +0000

    102.4 Lists - LinkedList/TestLinkedListAddAll.java

--- 06.2 java.util/102.4 Lists - LinkedList/TestLinkedListAddAll.java
+++ 06.2 java.util/102.4 Lists - LinkedList/TestLinkedListAddAll.java
@@ -5,15 +5,12 @@ public class TestLinkedListAddAll {
         // Create LinkedList instances
         LinkedList<String> c = new LinkedList<>();
         LinkedList<String> stack = new LinkedList<>();
-        
         // Adding elements to 'c'
         c.push("Zero");
         c.push("First");
         c.push("Second");
-        
         // Adding elements from 'c' to 'stack'
         stack.addAll(c);
-        
         // Getting and printing stack elements
         System.out.println("Stack after addition: " + stack);
     }
