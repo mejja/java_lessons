9149cd9f6cea52f638ba9ad32380563f58cefbfd
mejja <majorkiema.mk@gmail.com>
2025-12-06 19:36:18 +0000

    103.3 (Set - NavigableSet)/NavigableSet1.java

--- 06.2 java.util/103.3 (Set - NavigableSet)/NavigableSet1.java
+++ 06.2 java.util/103.3 (Set - NavigableSet)/NavigableSet1.java
@@ -1,18 +1,27 @@
 import java.util.NavigableSet;
 import java.util.TreeSet;
 
-public class NavigableSet1{
-	public static void main(String[] args){
-		NavigableSet<Long> navigableSet = new TreeSet<>();//instance
+/**
+ * Demonstrates basic usage of NavigableSet using TreeSet in Java.
+ * Shows how to add elements and retrieve the last (highest) element.
+ */
+public class NavigableSet1 {
 
-		navigableSet.add(88L);
-		navigableSet.add(11L);
-		navigableSet.add(77L);
-		navigableSet.add(22L);
-		navigableSet.add(99L);
+    public static void main(String[] args) {
+        // Create a NavigableSet of Long values using TreeSet
+        NavigableSet<Long> navigableSet = new TreeSet<>();
 
-		//Getting the last element in the list
-		System.out.println("NavigableSet: "+navigableSet);
-		System.out.println("The Last Item in List IS: "+navigableSet.last());
-	}
+        // Add elements to the NavigableSet
+        navigableSet.add(88L);
+        navigableSet.add(11L);
+        navigableSet.add(77L);
+        navigableSet.add(22L);
+        navigableSet.add(99L);
+
+        // Print the NavigableSet (automatically sorted in ascending order)
+        System.out.println("NavigableSet: " + navigableSet);
+
+        // Retrieve and print the last (highest) element
+        System.out.println("The Last Item in the Set is: " + navigableSet.last());
+    }
 }
