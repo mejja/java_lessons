3d7b5bc0a2e530302f0fca873f94102a50c5bd8d
mejja <majorkiema.mk@gmail.com>
2025-12-24 09:58:21 +0000

    06.2 java.util/107.3 (Equals)/EquivalencyTest1.java

--- 06.2 java.util/107.3 (Equals)/EquivalencyTest1.java
+++ 06.2 java.util/107.3 (Equals)/EquivalencyTest1.java
@@ -1,11 +1,21 @@
+/**
+ * Demonstrates string equivalency using the equals() method.
+ */
 public class EquivalencyTest1 {
-	public static void main(String[] args) {
-		
-		String a = "a";
-		
-		boolean result = a.equals(a);
-		
-		System.out.println("Result = "+result);
-		
-	}
+
+    public static void main(String[] args) {
+
+        // Declare and initialize a String literal
+        String a = "a";
+
+        /*
+         * The equals() method compares the content of the String.
+         * Since 'a' is compared with itself, the result is always true.
+         */
+        boolean result = a.equals(a);
+
+        // Print the result
+        System.out.println("Result = " + result);
+    }
 }
+
