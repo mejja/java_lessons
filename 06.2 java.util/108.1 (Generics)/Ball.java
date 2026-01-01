0fa4d5619ed9f5cb5cb337d52c5aa006884ecdf6
mejja <majorkiema.mk@gmail.com>
2026-01-01 17:36:47 +0000

    06.2 java.util/108.1 (Generics)/Ball.java

--- 06.2 java.util/108.1 (Generics)/Ball.java
+++ 06.2 java.util/108.1 (Generics)/Ball.java
@@ -2,26 +2,34 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-public class Ball{
-	@Override
-	public String toString(){
-		return "ball";
-	}
+/**
+ * Demonstrates iterating over values in a HashMap using an Iterator.
+ */
+public class Ball {
 
-	public static void main(String[] args){
-		Map<Integer,Ball> ballMap = new HashMap<>();
-		ballMap.put(1, new Ball());
-		ballMap.put(2, new Ball());
-		ballMap.put(3, new Ball());
-		ballMap.put(4, new Ball());
+    @Override
+    public String toString() {
+        return "ball";
+    }
 
-		//Iterator
-		 Iterator<Ball> iterator = ballMap.values().iterator();
+    public static void main(String[] args) {
 
-		//print 
-		while(iterator.hasNext()){
-			Ball ball = iterator.next();
-			System.out.println(ball.toString());
-		}
-	}
+        // Create a Map to store Ball objects with Integer keys
+        Map<Integer, Ball> ballMap = new HashMap<>();
+
+        // Add entries to the map
+        ballMap.put(1, new Ball());
+        ballMap.put(2, new Ball());
+        ballMap.put(3, new Ball());
+        ballMap.put(4, new Ball());
+
+        // Obtain an iterator over the map's values
+        Iterator<Ball> iterator = ballMap.values().iterator();
+
+        // Iterate through the values and print each Ball
+        while (iterator.hasNext()) {
+            Ball ball = iterator.next();
+            System.out.println(ball); // toString() is called implicitly
+        }
+    }
 }
