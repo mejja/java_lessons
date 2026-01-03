d414335049b637ffd443d25c2895d286ddbff5c0
mejja <majorkiema.mk@gmail.com>
2026-01-03 17:41:47 +0000

    06.2 java.util/108.1 (Generics)/GenericsEg2.java

--- 06.2 java.util/108.1 (Generics)/GenericsEg2.java
+++ 06.2 java.util/108.1 (Generics)/GenericsEg2.java
@@ -1,12 +1,22 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Demonstrates the use of generics with Map and HashMap in Java.
+ */
 public class GenericsEg2 {
+
     public static void main(String[] args) {
-        Map<Integer,String> strings = new HashMap<Integer,String>();
-  	  strings.put(1,"one");
-  	  strings.put(2,"two");
-   	 strings.put(3,"three");
-   	 System.out.println("strings = "+strings);
+
+        // Create a Map with Integer keys and String values (type-safe)
+        Map<Integer, String> strings = new HashMap<>();
+
+        // Add key-value pairs to the map
+        strings.put(1, "one");
+        strings.put(2, "two");
+        strings.put(3, "three");
+
+        // Print the contents of the map
+        System.out.println("strings = " + strings);
     }
 }
