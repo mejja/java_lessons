edd2c36f56fea15a8283b4b7004983eb75860898
mejja <majorkiema.mk@gmail.com>
2026-01-03 17:39:18 +0000

    06.2 java.util/108.1 (Generics)/GenericsEg1.java

--- 06.2 java.util/108.1 (Generics)/GenericsEg1.java
+++ 06.2 java.util/108.1 (Generics)/GenericsEg1.java
@@ -8,13 +8,13 @@ public class GenericsEg1 {
 
     public static void main(String[] args) {
 
-        // Create a List of String using generics (type-safe)
+        // Create a type-safe List of Strings using generics
         List<String> strings = new ArrayList<>();
 
         // Add String elements to the list
         strings.add("1");
         strings.add("3");
-        strings.add("3");  // Duplicate values are allowed in a List
+        strings.add("3"); // Duplicate values are allowed in a List
 
         // Print the contents of the list
         System.out.println("strings = " + strings);
