a1487ab6ced289e544512b5f494952d607ce74eb
mejja <majorkiema.mk@gmail.com>
2026-01-05 17:53:20 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists2.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists2.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists2.java
@@ -1,16 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 
- public class MoneyLists2 {
-	public static void main(String[] args) {
-		List<Money> list2 = new ArrayList<Money>();
-		
-		list2.add(new Money(Currency.USD, 10));
-		list2.add(new Money(Currency.USD, 10));
-		list2.add(new Money(Currency.USD, 10));
-		list2.add(new Money(Currency.USD, 10));
-		list2.add(new Money(Currency.USD, 10));
-	
-		System.out.println("list2 = "+list2);
-	}
+/**
+ * Demonstrates storing multiple Money objects in a List.
+ */
+public class MoneyLists2 {
+
+    public static void main(String[] args) {
+
+        // Create a List to store Money objects
+        List<Money> list2 = new ArrayList<>();
+
+        // Add Money objects to the list (duplicates are allowed in a List)
+        list2.add(new Money(Currency.USD, 10));
+        list2.add(new Money(Currency.USD, 10));
+        list2.add(new Money(Currency.USD, 10));
+        list2.add(new Money(Currency.USD, 10));
+        list2.add(new Money(Currency.USD, 10));
+
+        // Print the contents of the list
+        System.out.println("list2 = " + list2);
+    }
 }
