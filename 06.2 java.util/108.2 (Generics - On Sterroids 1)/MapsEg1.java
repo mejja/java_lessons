d2e0f47bbb8115bc73fafd64238b818704128dfa
mejja <majorkiema.mk@gmail.com>
2026-01-07 17:55:19 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/MapsEg1.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/MapsEg1.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/MapsEg1.java
@@ -1,15 +1,22 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Demonstrates the use of generics with Map in Java.
+ * The map associates a String key with a Money value.
+ */
 public class MapsEg1 {
-	public static void main(String[] args) {
-		
-		Map<String, Money> shareMap = new HashMap<String,Money>();
-		
-		
-		shareMap.put("Mr X",new Money(Currency.USD, 100));
-		
-		
-		System.out.println("shareMap = "+shareMap);
-	}
+
+    public static void main(String[] args) {
+
+        // Create a Map with String keys and Money values
+        Map<String, Money> shareMap = new HashMap<>();
+
+        // Add an entry to the map
+        shareMap.put("Mr X", new Money(Currency.USD, 100));
+
+        // Print the contents of the map
+        System.out.println("shareMap = " + shareMap);
+    }
 }
+
