bd14cc0fcab69f5abd833e4f9c99d00bfa331d09
mejja <majorkiema.mk@gmail.com>
2026-01-08 17:55:57 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/MapsEg2.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/MapsEg2.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/MapsEg2.java
@@ -1,16 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Demonstrates storing multiple entries in a Map using generics.
+ * Each key maps to a Money value.
+ */
 public class MapsEg2 {
-	public static void main(String[] args) {
-		
-		Map<String, Money> shareMap = new HashMap<String,Money>();
-		
-		shareMap.put("Mr Sly",new Money(Currency.USD, 1000));
-		shareMap.put("Mr Slicko",new Money(Currency.USD, 1000));
-		shareMap.put("Mr No3",new Money(Currency.USD, 1000));
-		shareMap.put("Mr John",new Money(Currency.USD, 1000));
-		
-		System.out.println("shareMap = "+shareMap);
-	}
+
+    public static void main(String[] args) {
+
+        // Create a Map with String keys and Money values
+        Map<String, Money> shareMap = new HashMap<>();
+
+        // Add multiple entries to the map
+        shareMap.put("Mr Sly", new Money(Currency.USD, 1000));
+        shareMap.put("Mr Slicko", new Money(Currency.USD, 1000));
+        shareMap.put("Mr No3", new Money(Currency.USD, 1000));
+        shareMap.put("Mr John", new Money(Currency.USD, 1000));
+
+        // Print the contents of the map
+        System.out.println("shareMap = " + shareMap);
+    }
 }
+
