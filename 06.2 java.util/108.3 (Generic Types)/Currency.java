2af023c752e67bb8b2b84b1facd05e95eb2d64dd
mejja <majorkiema.mk@gmail.com>
2026-01-09 18:09:09 +0000

    06.2 java.util/108.3 (Generic Types)/Currency.java

--- 06.2 java.util/108.3 (Generic Types)/Currency.java
+++ 06.2 java.util/108.3 (Generic Types)/Currency.java
@@ -1,6 +1,10 @@
-public enum Currency{
-	USD,
-	EUR,
-	ZAR,
-	GBP;
+/**
+ * Represents supported currency types.
+ */
+public enum Currency {
+
+    USD, // United States Dollar
+    EUR, // Euro
+    ZAR, // South African Rand
+    GBP  // British Pound Sterling
 }
