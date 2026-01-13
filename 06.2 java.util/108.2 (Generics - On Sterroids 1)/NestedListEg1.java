46bc4aaec24ac5d57e9165730725c676c70a725c
mejja <majorkiema.mk@gmail.com>
2026-01-13 18:00:57 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/NestedListEg1.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/NestedListEg1.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/NestedListEg1.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Demonstrates the use of a nested List (List of Lists) with generics.
+ */
 public class NestedListEg1 {
-	public static void main(String[] args) {
-		
-		List<List<Money>> outerList = new ArrayList<List<Money>>();
-		
-		List<Money> innerList = new ArrayList<Money>();
-		
-		innerList.add(new Money(Currency.USD, 100));
-		outerList.add(innerList);
-		
-		System.out.println("outerList = "+outerList);
-	}
+
+    public static void main(String[] args) {
+
+        // Create the outer list that will hold inner lists of Money objects
+        List<List<Money>> outerList = new ArrayList<>();
+
+        // Create an inner list of Money objects
+        List<Money> innerList = new ArrayList<>();
+
+        // Add a Money object to the inner list
+        innerList.add(new Money(Currency.USD, 100));
+
+        // Add the inner list to the outer list
+        outerList.add(innerList);
+
+        // Print the nested list structure
+        System.out.println("outerList = " + outerList);
+    }
 }
+
