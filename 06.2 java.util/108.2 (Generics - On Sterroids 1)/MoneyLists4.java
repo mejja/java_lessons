b6592a43e48b36ce1f2baa0ba159522ff7b0a4a2
mejja <majorkiema.mk@gmail.com>
2026-01-14 18:01:38 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists4.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists4.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists4.java
@@ -1,29 +1,42 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MoneyLists4{
-	public static void main(String[] args){
-		List<Money> list4 = new ArrayList<Money>();
+/**
+ * Demonstrates storing Money objects with multiple currencies in a List.
+ * Duplicate entries are allowed.
+ */
+public class MoneyLists4 {
 
-		list4.add(new Money(Currency.USD, 10));
-		list4.add(new Money(Currency.USD, 10));
-		list4.add(new Money(Currency.USD, 10));
-		list4.add(new Money(Currency.USD, 10));
-		list4.add(new Money(Currency.USD, 10));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.GBP, 5));
-		list4.add(new Money(Currency.ZAR, 1));
-		list4.add(new Money(Currency.ZAR, 1));
-		list4.add(new Money(Currency.ZAR, 1));
-		list4.add(new Money(Currency.ZAR, 1));
-		list4.add(new Money(Currency.ZAR, 1));
-		list4.add(new Money(Currency.ZAR, 1));
+    public static void main(String[] args) {
 
-		System.out.println("list4 = "+list4);
-	}
+        // Create a List to hold Money objects
+        List<Money> list4 = new ArrayList<>();
+
+        // Add USD Money objects
+        list4.add(new Money(Currency.USD, 10));
+        list4.add(new Money(Currency.USD, 10));
+        list4.add(new Money(Currency.USD, 10));
+        list4.add(new Money(Currency.USD, 10));
+        list4.add(new Money(Currency.USD, 10));
+
+        // Add GBP Money objects
+        list4.add(new Money(Currency.GBP, 5));
+        list4.add(new Money(Currency.GBP, 5));
+        list4.add(new Money(Currency.GBP, 5));
+        list4.add(new Money(Currency.GBP, 5));
+        list4.add(new Money(Currency.GBP, 5));
+        list4.add(new Money(Currency.GBP, 5));
+        list4.add(new Money(Currency.GBP, 5));
+
+        // Add ZAR Money objects
+        list4.add(new Money(Currency.ZAR, 1));
+        list4.add(new Money(Currency.ZAR, 1));
+        list4.add(new Money(Currency.ZAR, 1));
+        list4.add(new Money(Currency.ZAR, 1));
+        list4.add(new Money(Currency.ZAR, 1));
+        list4.add(new Money(Currency.ZAR, 1));
+
+        // Print the contents of the list
+        System.out.println("list4 = " + list4);
+    }
 }
