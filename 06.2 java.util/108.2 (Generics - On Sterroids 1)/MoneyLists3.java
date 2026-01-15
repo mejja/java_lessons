4bc2ea01b0c1f8296e3200d6e9bb7bbfa602cc9e
mejja <majorkiema.mk@gmail.com>
2026-01-15 18:02:21 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists3.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists3.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists3.java
@@ -1,23 +1,34 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MoneyLists3{
-	public static void main(String[] args){
-		List<Money> list3 = new ArrayList<Money>();
+/**
+ * Demonstrates storing multiple Money objects with different currencies
+ * in a List. Duplicates are allowed.
+ */
+public class MoneyLists3 {
 
-		list3.add(new Money(Currency.USD, 10));
-		list3.add(new Money(Currency.USD, 10));
-		list3.add(new Money(Currency.USD, 10));
-		list3.add(new Money(Currency.USD, 10));
-		list3.add(new Money(Currency.USD, 10));
-		list3.add(new Money(Currency.GBP, 5));
-		list3.add(new Money(Currency.GBP, 5));
-		list3.add(new Money(Currency.GBP, 5));
-		list3.add(new Money(Currency.GBP, 5));
-		list3.add(new Money(Currency.GBP, 5));
-		list3.add(new Money(Currency.GBP, 5));
-		list3.add(new Money(Currency.GBP, 5));
+    public static void main(String[] args) {
 
-		System.out.println("list3 = "+list3);
-	}
+        // Create a List to hold Money objects
+        List<Money> list3 = new ArrayList<>();
+
+        // Add USD Money objects
+        list3.add(new Money(Currency.USD, 10));
+        list3.add(new Money(Currency.USD, 10));
+        list3.add(new Money(Currency.USD, 10));
+        list3.add(new Money(Currency.USD, 10));
+        list3.add(new Money(Currency.USD, 10));
+
+        // Add GBP Money objects
+        list3.add(new Money(Currency.GBP, 5));
+        list3.add(new Money(Currency.GBP, 5));
+        list3.add(new Money(Currency.GBP, 5));
+        list3.add(new Money(Currency.GBP, 5));
+        list3.add(new Money(Currency.GBP, 5));
+        list3.add(new Money(Currency.GBP, 5));
+        list3.add(new Money(Currency.GBP, 5));
+
+        // Print the contents of the list
+        System.out.println("list3 = " + list3);
+    }
 }
