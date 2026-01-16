96d860e4a2b5f9fa2e34237742090a0cd5aa12b9
mejja <majorkiema.mk@gmail.com>
2026-01-16 18:03:03 +0000

    06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists1.java

--- 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists1.java
+++ 06.2 java.util/108.2 (Generics - On Sterroids 1)/MoneyLists1.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
- public class MoneyLists1 {
-	public static void main(String[] args) {
-		List<Money> list1 = new ArrayList<Money>();
-		
-		list1.add(new Money(Currency.GBP, 20));
-		list1.add(new Money(Currency.GBP, 20));
-		list1.add(new Money(Currency.GBP, 20));
-		
-		System.out.println("list1 = "+list1);
-	}
+/**
+ * Demonstrates storing objects in a List using generics.
+ */
+public class MoneyLists1 {
+
+    public static void main(String[] args) {
+
+        // Create a List to store Money objects
+        List<Money> list1 = new ArrayList<>();
+
+        // Add Money objects to the list (duplicates are allowed in a List)
+        list1.add(new Money(Currency.GBP, 20));
+        list1.add(new Money(Currency.GBP, 20));
+        list1.add(new Money(Currency.GBP, 20));
+
+        // Print the contents of the list
+        System.out.println("list1 = " + list1);
+    }
 }
