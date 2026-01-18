4d078ff90fab94ae9de65bc6a34918fada05cdff
mejja <majorkiema.mk@gmail.com>
2026-01-18 18:11:07 +0000

    06.2 java.util/108.3 (Generic Types)/Safe.java

--- 06.2 java.util/108.3 (Generic Types)/Safe.java
+++ 06.2 java.util/108.3 (Generic Types)/Safe.java
@@ -1,4 +1,24 @@
-public interface Safe<T extends Money>{
-	int lockIn(T target);
-	T getFromSafe(int key);	
+/**
+ * A generic interface representing a Safe that can store and retrieve
+ * objects of type T, where T must be a subclass of Money.
+ *
+ * @param <T> the type of object stored in the safe (must extend Money)
+ */
+public interface Safe<T extends Money> {
+
+    /**
+     * Stores the given object in the safe.
+     *
+     * @param target the object to be stored
+     * @return a unique key used to retrieve the object later
+     */
+    int lockIn(T target);
+
+    /**
+     * Retrieves an object from the safe using the provided key.
+     *
+     * @param key the key associated with the stored object
+     * @return the object retrieved from the safe
+     */
+    T getFromSafe(int key);
 }
