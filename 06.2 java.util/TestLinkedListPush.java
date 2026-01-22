d057f0277fb8709ab56d45a745505bdd6874da8e
mejja <majorkiema.mk@gmail.com>
2026-01-22 20:44:16 +0000

    06.2 java.util/TestLinkedListPush.java

--- 06.2 java.util/TestLinkedListPush.java
+++ 06.2 java.util/TestLinkedListPush.java
@@ -1,14 +1,19 @@
-import java.util.LinkedList;
+import java.util.LinkedList; // Import LinkedList class from java.util package
 
-public class TestLinkedListPush{
-	public static void main(String[] args){
-		LinkedList<String> stack = new LinkedList<>();
+public class TestLinkedListPush {
 
-		//Push elements
-		stack.push("First");
-		stack.push("Second");
+    public static void main(String[] args) {
 
-		//Print stack elements
-		System.out.println("Stack elements after pushing: "+ stack);
-	}
+        // Create a LinkedList to be used as a stack (LIFO data structure)
+        LinkedList<String> stack = new LinkedList<>();
+
+        // Push elements onto the stack
+        // push() inserts elements at the beginning (top) of the stack
+        stack.push("First");   // Stack: [First]
+        stack.push("Second");  // Stack: [Second, First]
+
+        // Print the stack elements after pushing
+        // The most recently pushed element appears first
+        System.out.println("Stack elements after pushing: " + stack);
+    }
 }
